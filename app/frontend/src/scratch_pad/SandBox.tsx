85ecfee861e43e657d86a9f9852cdf2e595e1fbd
Alexander James Phillips <ajp.global@gmail.com>
2021-04-21 23:57:50 +0100

    Simplify EditableCustomDateTime component again

--- app/frontend/src/scratch_pad/SandBox.tsx
+++ app/frontend/src/scratch_pad/SandBox.tsx
@@ -8,9 +8,12 @@ export function SandBox ()
 {
     const [custom_date, set_custom_date] = useState<Date | undefined>(undefined)
 
-    return <EditableCustomDateTime
-        invariant_value={undefined} //new Date("2021-04-15 14:02")}
-        value={custom_date}
-        on_change={custom_date => set_custom_date(custom_date)}
-    />
+    return <div>
+        EditableCustomDateTime <div onClick={() => set_custom_date(new Date("2000-01-01"))}>testing</div>
+        <EditableCustomDateTime
+            invariant_value={undefined} //new Date("2021-04-15 14:02")}
+            value={custom_date}
+            on_change={custom_date => set_custom_date(custom_date)}
+        />
+    </div>
 }
