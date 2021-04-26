c3fd19472599e1d17855524b43fc1476dc4b17f0
Alexander James Phillips <ajp.global@gmail.com>
2021-04-26 16:59:15 +0100

    Fix bug with add and cancel

--- app/frontend/src/form/editable_list/EditableList.tsx
+++ app/frontend/src/form/editable_list/EditableList.tsx
@@ -2,7 +2,7 @@ import { h } from "preact"
 import { useState } from "preact/hooks"
 
 import { ExpandableList, ExpandableListProps, ListContentProps } from "./ExpandableList"
-import { NewItemForm, new_item_form } from "./new_item_form"
+import { NewItemForm } from "./NewItemForm"
 import { FactoryRenderListContentProps, factory_render_list_content } from "./render_list_content"
 
 
@@ -19,16 +19,6 @@ export function EditableList <T> (props: EditableListProps<T>)
 {
     const [new_item, set_new_item] = useState<T | undefined>(undefined)
 
-
-    const new_item_form_content = new_item_form({
-        new_item,
-        item_top_props: props.item_top_props,
-        item_descriptor: props.item_descriptor,
-        set_new_item,
-        add_item: () => new_item && props.update_items([...props.items, new_item])
-    })
-
-
     const render_list_content = factory_render_list_content({
         items: props.items,
         get_id: props.get_id,
@@ -41,17 +31,30 @@ export function EditableList <T> (props: EditableListProps<T>)
 
 
     return <ExpandableList
-        {...props}
-        content={(list_content_props: ListContentProps) =>
-        {
-            return <div>
-                {new_item_form_content}
-                {render_list_content(list_content_props)}
-            </div>
-        }}
         on_click_new_item={() => {
             const item = props.prepare_new_item()
             set_new_item(item)
         }}
+
+        content={(list_content_props: ListContentProps) =>
+        {
+            return <div>
+                <NewItemForm
+                    new_item={new_item}
+                    set_new_item={set_new_item}
+                    item_top_props={props.item_top_props}
+                    item_descriptor={props.item_descriptor}
+                    add_item={new_item =>
+                    {
+                        props.update_items([...props.items, new_item])
+                        set_new_item(undefined)
+                    }}
+                />
+
+                {render_list_content(list_content_props)}
+            </div>
+        }}
+
+        {...props}
     />
 }
---
+++ app/frontend/src/form/editable_list/NewItemForm.tsx
@@ -0,0 +1,75 @@
+import { h } from "preact"
+import { useEffect, useState } from "preact/hooks"
+
+import { Button } from "../../sharedf/Button"
+import { EditableListEntry, EditableListEntryTopProps } from "./EditableListEntry"
+
+
+
+export interface NewItemForm<U>
+{
+    new_item: U | undefined
+    set_new_item: (new_item: U | undefined) => void
+    item_descriptor: string
+    item_top_props: EditableListEntryTopProps<U>
+    add_item: (new_item: U) => void
+}
+
+
+
+export function NewItemForm <T> (props: NewItemForm<T>)
+{
+    const { new_item, set_new_item, item_descriptor, item_top_props, add_item } = props
+
+    const [adding_item, set_adding_item] = useState(false)
+
+    // call add_item if necessary
+    // Using effect because various form elements
+    // will issue on_change events when they blur.  And they will blur when the
+    // add (or cancel) buttons are pressed.  If we do not allow them to finish
+    // then the item added will be a stale version.
+    useEffect(() =>
+    {
+        if (!adding_item) return
+        if (new_item) add_item(new_item)
+        set_adding_item(false)
+    }, [add_item, adding_item])
+
+
+    if (!new_item) return null
+
+
+    return <div onClick={e => e.stopPropagation()}>
+        <hr />
+        <EditableListEntry
+            item={new_item}
+            {...item_top_props}
+            expanded={true}
+            disable_collapsable={true}
+            on_change={item => {
+                set_new_item(item)
+            }}
+        />
+
+        <Button
+            value={`Add ${item_descriptor}`}
+            on_pointer_down={() =>
+            {
+                const input = document.activeElement as HTMLElement | HTMLInputElement | null
+                if (input && input.blur) input.blur()
+                set_adding_item(true)
+            }}
+        />
+        <Button
+            value="Cancel"
+            extra_class_names="button_warning"
+            on_pointer_down={() => {
+                const input = document.activeElement as HTMLElement | HTMLInputElement | null
+                if (input && input.blur) input.blur()
+                set_new_item(undefined)
+            }}
+        />
+        <br />
+        <hr />
+    </div>
+}
