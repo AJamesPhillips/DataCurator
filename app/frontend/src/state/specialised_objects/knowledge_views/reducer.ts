51d71fc50e61bea8d3d7bfbac691d6981045be34
Alexander James Phillips <ajp.global@gmail.com>
2021-04-28 22:43:05 +0100

    Always add new wcomponents to the base KnowledgeView

--- app/frontend/src/state/specialised_objects/knowledge_views/reducer.ts
+++ app/frontend/src/state/specialised_objects/knowledge_views/reducer.ts
@@ -4,7 +4,7 @@ import type { KnowledgeView, KnowledgeViewWComponentEntry } from "../../../share
 import { upsert_entry, replace_element } from "../../../utils/list"
 import { update_substate } from "../../../utils/update_state"
 import type { RootState } from "../../State"
-import { get_knowledge_view_from_state } from "../accessors"
+import { get_base_knowledge_view, get_knowledge_view_from_state } from "../accessors"
 import { is_upsert_wcomponent } from "../wcomponents/actions"
 import {
     is_upsert_knowledge_view,
@@ -31,6 +31,8 @@ export const knowledge_views_reducer = (state: RootState, action: AnyAction): Ro
         {
             const entry: KnowledgeViewWComponentEntry = { ...add_to_knowledge_view.position }
             state = handle_upsert_knowledge_view_entry(state, add_to_knowledge_view.id, wcomponent.id, entry)
+
+            state = add_wcomponent_to_base_knowledge_view(state, wcomponent.id, entry)
         }
     }
 
@@ -75,15 +77,20 @@ function handle_upsert_knowledge_view_entry (state: RootState, knowledge_view_id
 
     if (!knowledge_view) throw new Error(`Could not find knowledge_view for id: "${knowledge_view_id}"`)
 
-    const new_knowledge_view = update_substate(knowledge_view, "wc_id_map", wcomponent_id, entry)
-
-    const knowledge_views = state.specialised_objects.knowledge_views
-    const new_knowledge_views = replace_element(knowledge_views, new_knowledge_view, kv => kv.id === knowledge_view_id)
-
-    return update_substate(state, "specialised_objects", "knowledge_views", new_knowledge_views)
+    return add_wcomponent_entry_to_knowledge_view(state, knowledge_view, wcomponent_id, entry)
 }
 
 
+
+function add_wcomponent_entry_to_knowledge_view(state: RootState, knowledge_view: KnowledgeView, wcomponent_id: string, entry: KnowledgeViewWComponentEntry): RootState
+{
+    const new_knowledge_view = update_substate(knowledge_view, "wc_id_map", wcomponent_id, entry)
+
+    const knowledge_views = state.specialised_objects.knowledge_views
+    const new_knowledge_views = replace_element(knowledge_views, new_knowledge_view, kv => kv.id === knowledge_view.id)
+
+    return update_substate(state, "specialised_objects", "knowledge_views", new_knowledge_views)
+}
 
 function handle_delete_knowledge_view_entry (state: RootState, knowledge_view_id: string, wcomponent_id: string)
 {
@@ -102,4 +109,26 @@ function handle_delete_knowledge_view_entry (state: RootState, knowledge_view_id
     const new_knowledge_views = replace_element(knowledge_views, new_knowledge_view, kv => kv.id === knowledge_view_id)
 
     return update_substate(state, "specialised_objects", "knowledge_views", new_knowledge_views)
+}
+
+
+
+function add_wcomponent_to_base_knowledge_view(state: RootState, wcomponent_id: string, entry: KnowledgeViewWComponentEntry): RootState
+{
+    const { base_knowledge_view } = get_base_knowledge_view(state)
+    if (!base_knowledge_view)
+    {
+        console.error("There should always be a base knowledge view once wcomponents are being added")
+        return state
+    }
+
+    const existing_entry = base_knowledge_view.wc_id_map[wcomponent_id]
+
+    if (existing_entry)
+    {
+        // Nothing needs to be done
+        return state
+    }
+
+    return add_wcomponent_entry_to_knowledge_view(state, base_knowledge_view, wcomponent_id, entry)
 }
