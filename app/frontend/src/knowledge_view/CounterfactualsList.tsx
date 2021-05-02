13592b175a4312815253667202de404a6cb74771
Alexander James Phillips <ajp.global@gmail.com>
2021-05-02 16:05:55 +0100

    Add back CounterfactualsList.tsx with lower case F

---
+++ app/frontend/src/knowledge_view/CounterfactualsList.tsx
@@ -0,0 +1,81 @@
+import { h } from "preact"
+
+import { EditableTextSingleLine } from "../form/EditableTextSingleLine"
+import { ExpandableListWithAddButton } from "../form/editable_list/ExpandableListWithAddButton"
+import { factory_render_list_content } from "../form/editable_list/render_list_content"
+import type { CounterfactualLayer } from "../shared/models/interfaces/counterfactual"
+import { date2str } from "../shared/utils/date_helpers"
+import { Link } from "../utils/Link"
+import { create_new_counterfactual_layer } from "./create_new_counterfactual_layer"
+
+
+
+interface OwnProps
+{
+    counterfactual_layers: CounterfactualLayer[]
+}
+
+export function CounterfactualsList (props: OwnProps)
+{
+    const { counterfactual_layers } = props
+
+    return <ExpandableListWithAddButton
+        items_count={counterfactual_layers.length}
+        on_click_new_item={() =>
+        {
+            const knowledge_view = create_new_counterfactual_layer({ title: make_default_title() })
+            // props.upsert_knowledge_view({ knowledge_view })
+        }}
+        content={factory_render_list_content({
+            items: counterfactual_layers,
+            get_id: kv => kv.id,
+            update_items: new_cfls =>
+            {
+                const changed_kv = new_cfls.find((new_cfl, index) => counterfactual_layers[index] !== new_cfl)
+                if (!changed_kv) return
+                // props.upsert_knowledge_view({ knowledge_view: changed_kv })
+            },
+
+            item_top_props: { get_summary, get_details },
+
+            item_descriptor: "Counterfactual",
+        })}
+        item_descriptor="Counterfactual"
+        disable_collapsed={true}
+    />
+}
+
+
+
+const make_default_title = () => date2str(new Date(), "yyyy-MM-dd")
+
+
+
+function get_summary (counterfactual_layer: CounterfactualLayer, on_change: (new_cfl: CounterfactualLayer) => void)
+{
+    return <Link
+        route={undefined}
+        sub_route={undefined}
+        item_id={undefined}
+        args={{ view: "knowledge", subview_id: counterfactual_layer.id }}
+        selected_on={new Set(["route", "args.subview_id"])}
+    >
+        {counterfactual_layer.title}
+    </Link>
+}
+
+
+
+function get_details (counterfactual_layer: CounterfactualLayer, on_change: (new_cfl: CounterfactualLayer) => void)
+{
+    return <div>
+        <EditableTextSingleLine
+            placeholder="Title..."
+            value={counterfactual_layer.title}
+            on_change={new_title => {
+                const title = new_title || make_default_title()
+                on_change({ ...counterfactual_layer, title })
+            }}
+        />
+    </div>
+}
