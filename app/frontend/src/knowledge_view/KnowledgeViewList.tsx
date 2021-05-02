bf8531aad174e2cee61d7cdf90f57d25e8a4911e
Alexander James Phillips <ajp.global@gmail.com>
2021-05-02 13:54:55 +0100

    Simplify rendering knowledge views list

--- app/frontend/src/knowledge_view/KnowledgeViewList.tsx
+++ app/frontend/src/knowledge_view/KnowledgeViewList.tsx
@@ -25,28 +25,27 @@ function _KnowledgeViewList (props: Props)
 {
     const { ready, base_knowledge_view, other_knowledge_views } = props
 
-    const base_title = base_knowledge_view && base_knowledge_view.title !== "Base"
+    if (!base_knowledge_view)
+    {
+        return <div style={{ cursor: "progress" }}>
+            {ready ? "Automatically creating base knowledge view..." : "Loading..." }
+        </div>
+    }
+
+
+    const base_title = base_knowledge_view.title !== "Base"
         ? `Base (${base_knowledge_view.title})`
         : "Base"
 
+
+    const knowledge_views = [
+        { id: base_knowledge_view.id, title: base_title},
+        ...other_knowledge_views,
+    ]
+
+
     return <div>
-        {!base_knowledge_view && <div style={{ cursor: "progress" }}>
-            {ready ? "Automatically creating base knowledge view..." : "Loading..." }
-        </div>}
-
-        {base_knowledge_view && <Link
-            route={undefined}
-            sub_route={undefined}
-            item_id={undefined}
-            args={{ view: "knowledge", subview_id: base_knowledge_view.id }}
-            selected_on={new Set(["route", "args.subview_id"])}
-        >
-            {base_title}
-        </Link>}
-
-        <br />
-
-        {other_knowledge_views.map(({ id, title }) => <Link
+        {knowledge_views.map(({ id, title }) => <Link
             route={undefined}
             sub_route={undefined}
             item_id={undefined}
