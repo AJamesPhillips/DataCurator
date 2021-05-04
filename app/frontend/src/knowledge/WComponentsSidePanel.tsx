9c20150d4f0922a5fd479c7b8ffcbf871c10a42f
Alexander James Phillips <ajp.global@gmail.com>
2021-05-04 11:02:54 +0100

    Show a link to already selected components under the components tab

--- app/frontend/src/knowledge/WComponentsSidePanel.tsx
+++ app/frontend/src/knowledge/WComponentsSidePanel.tsx
@@ -7,6 +7,7 @@ import { CreateNewWComponent } from "./CreateNewWComponent"
 import { WComponentForm } from "./wcomponent_form/WComponentForm"
 import { WComponentMultipleForm } from "./WComponentMultipleForm"
 import { WComponentsList } from "./WComponentsList"
+import { LinkButton } from "../utils/Link"
 
 
 
@@ -18,8 +19,9 @@ const map_state = (state: RootState) =>
     const sub_route = state.routing.sub_route
     const id = state.routing.item_id
     const wcomponent = get_wcomponent_from_state(state, id)
+    const selected_ids = state.meta_wcomponents.selected_wcomponent_ids_list
 
-    return { ready, sub_route, id, wcomponent }
+    return { ready, sub_route, id, wcomponent, selected_ids }
 }
 
 
@@ -41,6 +43,18 @@ function _WComponentsSidePanel (props: Props)
 
     if (!props.id) return <div>
         <CreateNewWComponent />
+
+        <hr />
+
+        {props.selected_ids.length > 0 && <p>
+            <LinkButton
+                name={`View ${props.selected_ids.length} component(s) already selected`}
+                route="wcomponents"
+                sub_route={props.selected_ids.length > 1 ? "wcomponents_edit_multiple" : undefined}
+                item_id={props.selected_ids.length === 1 ? props.selected_ids[0] : undefined}
+                args={undefined}
+            />
+        </p>}
 
         <p>
             Or select a component to view
