67c6fc5663604a5f6de158a4b5dcf67eaf5a4f75
Alexander James Phillips <ajp.global@gmail.com>
2021-05-24 21:35:29 +0100

    Add options to change position by a small step

--- app/frontend/src/form/EditablePosition.tsx
+++ app/frontend/src/form/EditablePosition.tsx
@@ -1,6 +1,6 @@
 import { h } from "preact"
 import type { CanvasPoint } from "../canvas/interfaces"
-import { round_coordinate_small_step } from "../canvas/position_utils"
+import { grid_small_step, round_coordinate_small_step } from "../canvas/position_utils"
 import { h_step, v_step } from "../state/display/display"
 
 import { EditableNumber } from "./EditableNumber"
@@ -33,14 +33,24 @@ export function EditablePosition (props: OwnProps)
         <input
             type="button"
             value="&#8592;"
+            onClick={() => update({ left: round_coordinate_small_step(left - grid_small_step) })}
+        />
+        <input
+            type="button"
+            value="&#8592;&#8592;"
             onClick={() => update({ left: round_coordinate_small_step(left - h_step) })}
         />
         <input
             type="button"
-            value="&#8594;"
+            value="&#8594;&#8594;"
             onClick={() => update({ left: round_coordinate_small_step(left + h_step) })}
         />
-
+        <input
+            type="button"
+            value="&#8594;"
+            onClick={() => update({ left: round_coordinate_small_step(left + grid_small_step) })}
+        />
+        <br />
         Top: <EditableNumber
             placeholder="Top"
             value={top}
@@ -50,12 +60,22 @@ export function EditablePosition (props: OwnProps)
         <input
             type="button"
             value="&#8593;"
+            onClick={() => update({ top: round_coordinate_small_step(top - grid_small_step) })}
+        />
+        <input
+            type="button"
+            value="&#8593;&#8593;"
             onClick={() => update({ top: round_coordinate_small_step(top - v_step) })}
         />
         <input
             type="button"
-            value="&#8595;"
+            value="&#8595;&#8595;"
             onClick={() => update({ top: round_coordinate_small_step(top + v_step) })}
+        />
+        <input
+            type="button"
+            value="&#8595;"
+            onClick={() => update({ top: round_coordinate_small_step(top + grid_small_step) })}
         />
     </div>
 }
