4bb4e6ac9d2b8944d36ea7390ca318e77fa011bc
Alexander James Phillips <ajp.global@gmail.com>
2021-05-31 11:30:49 +0100

    Actions and causal links return statev2 value

--- app/frontend/src/shared/wcomponent/get_wcomponent_state_value.ts
+++ app/frontend/src/shared/wcomponent/get_wcomponent_state_value.ts
@@ -1,6 +1,6 @@
 import type { CurrentValuePossibility, VAPsRepresent } from "./interfaces/generic_value"
-import { WComponent, wcomponent_has_VAP_sets, wcomponent_is_statev1, wcomponent_is_statev2 } from "./interfaces/SpecialisedObjects"
-import type { WComponentNodeState, WComponentNodeStateV2, WComponentStateV2SubType } from "./interfaces/state"
+import { WComponent, WComponentNode, wcomponent_has_VAP_sets, wcomponent_is_statev1, wcomponent_is_statev2, wcomponent_should_have_state_VAP_sets } from "./interfaces/SpecialisedObjects"
+import type { HasVAPSets, WComponentNodeState, WComponentNodeStateV2, WComponentStateV2SubType } from "./interfaces/state"
 import type { WComponentCounterfactuals } from "./interfaces/uncertainty/uncertainty"
 import { get_created_at_ms } from "./utils_datetime"
 import { get_VAP_set_possible_values } from "./value_and_prediction/get_value"
@@ -19,7 +19,19 @@ export function get_wcomponent_state_value (args: GetWcomponentStateValueArgs): 
     const { wcomponent, wc_counterfactuals, created_at_ms, sim_ms } = args
 
     if (wcomponent_is_statev1(wcomponent)) return get_wcomponent_statev1_value(wcomponent, created_at_ms, sim_ms)
-    if (wcomponent_is_statev2(wcomponent)) return get_wcomponent_statev2_value({ wcomponent, wc_counterfactuals, created_at_ms, sim_ms })
+    else if (wcomponent_should_have_state_VAP_sets(wcomponent))
+    {
+        const subtype = wcomponent_is_statev2(wcomponent) ? wcomponent.subtype : "boolean"
+        const VAPs_represent = subtype_to_VAPsRepresent(subtype)
+
+        return get_VAP_set_possible_values({
+            values_and_prediction_sets: wcomponent.values_and_prediction_sets,
+            VAPs_represent,
+            wc_counterfactuals,
+            created_at_ms,
+            sim_ms,
+        })
+    }
     else return []
 }
 
@@ -43,30 +55,6 @@ function get_wcomponent_statev1_value (wcomponent: WComponentNodeState, created_
     }
 
     return [possibility]
-}
-
-
-
-interface GetWcomponentStatev2ValueArgs
-{
-    wcomponent: WComponentNodeStateV2
-    wc_counterfactuals: WComponentCounterfactuals | undefined
-    created_at_ms: number
-    sim_ms: number
-}
-export function get_wcomponent_statev2_value (args: GetWcomponentStatev2ValueArgs): CurrentValuePossibility[]
-{
-    const { wcomponent, wc_counterfactuals, created_at_ms, sim_ms } = args
-
-    const VAPs_represent = subtype_to_VAPsRepresent(wcomponent.subtype)
-
-    return get_VAP_set_possible_values({
-        values_and_prediction_sets: wcomponent.values_and_prediction_sets,
-        VAPs_represent,
-        wc_counterfactuals,
-        created_at_ms,
-        sim_ms,
-    })
 }
 
 
