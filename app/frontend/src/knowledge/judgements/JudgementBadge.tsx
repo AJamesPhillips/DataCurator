a8a413fc841840f8c90a396493a2948133957a7c
Alexander James Phillips <ajp.global@gmail.com>
2021-06-16 11:24:36 +0100

    JudgementsBadge links move to judgements

--- app/frontend/src/knowledge/judgements/JudgementBadge.tsx
+++ app/frontend/src/knowledge/judgements/JudgementBadge.tsx
@@ -3,6 +3,7 @@ import { h } from "preact"
 import "./JudgementBadge.css"
 import type { JudgementValue } from "./calculate_judgement_value"
 import { format_wcomponent_url } from "../../shared/wcomponent/rich_text/templates"
+import type { CanvasPoint } from "../../canvas/interfaces"
 
 
 
@@ -10,6 +11,7 @@ interface OwnProps
 {
     judgement: JudgementValue
     judgement_or_objective_id?: string
+    position?: CanvasPoint
 }
 
 // Refactor this to be hidden inside JudgementBadgeC
@@ -20,7 +22,7 @@ export function JudgementBadge (props: OwnProps)
 
     if (!props.judgement_or_objective_id) return <div className={class_name}></div>
 
-    const href = format_wcomponent_url("", props.judgement_or_objective_id)
+    const href = format_wcomponent_url("", props.judgement_or_objective_id, props.position)
 
     return <a
         className={class_name}
--- app/frontend/src/knowledge/judgements/JudgementBadgeC.tsx
+++ app/frontend/src/knowledge/judgements/JudgementBadgeC.tsx
@@ -7,7 +7,8 @@ import { get_wcomponent_counterfactuals } from "../../state/derived/accessor"
 import type { RootState } from "../../state/State"
 import { calculate_judgement_value } from "./calculate_judgement_value"
 import { JudgementBadge } from "./JudgementBadge"
-import { get_wcomponent_from_state } from "../../state/specialised_objects/accessors"
+import { get_current_UI_knowledge_view_from_state, get_wcomponent_from_state } from "../../state/specialised_objects/accessors"
+import type { CanvasPoint } from "../../canvas/interfaces"
 
 
 
@@ -23,11 +24,19 @@ const map_state = (state: RootState, own_props: OwnProps) => {
 
     let target_wcomponent: WComponent | undefined = undefined
     let target_counterfactuals: WComponentCounterfactuals | undefined = undefined
+    let position: CanvasPoint | undefined = undefined
+
     if (wcomponent && wcomponent_is_judgement_or_objective(wcomponent))
     {
         const target_id = wcomponent.judgement_target_wcomponent_id
         target_wcomponent = get_wcomponent_from_state(state, target_id)
         target_counterfactuals = get_wcomponent_counterfactuals(state, target_id)
+
+        const kv = get_current_UI_knowledge_view_from_state(state)
+        if (kv)
+        {
+            position = kv.derived_wc_id_map[wcomponent.id]
+        }
     }
 
     return {
@@ -36,6 +45,7 @@ const map_state = (state: RootState, own_props: OwnProps) => {
         target_counterfactuals,
         created_at_ms: state.routing.args.created_at_ms,
         sim_ms: state.routing.args.sim_ms,
+        position,
     }
 }
 
@@ -48,13 +58,13 @@ type Props = ConnectedProps<typeof connector> & OwnProps
 // Refactor this file to hide JudgementBadge inside it
 function _JudgementBadgeC (props: Props)
 {
-    const { judgement_or_objective_id: judgement_id, wcomponent, target_wcomponent, target_counterfactuals, created_at_ms, sim_ms } = props
+    const { judgement_or_objective_id: judgement_id, wcomponent, target_wcomponent, target_counterfactuals, created_at_ms, sim_ms, position } = props
 
     if (!wcomponent || !target_wcomponent || !wcomponent_is_judgement_or_objective(wcomponent)) return null
 
     const judgement_value = calculate_judgement_value({ wcomponent, target_wcomponent, target_counterfactuals, created_at_ms, sim_ms })
 
-    return <JudgementBadge judgement={judgement_value} judgement_or_objective_id={judgement_id} />
+    return <JudgementBadge judgement={judgement_value} judgement_or_objective_id={judgement_id} position={position} />
 }
 
 export const JudgementBadgeC = connector(_JudgementBadgeC) as FunctionalComponent<OwnProps>
