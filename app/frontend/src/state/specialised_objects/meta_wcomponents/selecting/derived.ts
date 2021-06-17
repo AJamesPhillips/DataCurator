c8883594ac9764d02539b130b37b22e618e81a5b
Alexander James Phillips <ajp.global@gmail.com>
2021-06-17 15:56:53 +0100

    Simplify which wcomponent_ids remain selected on route change

--- app/frontend/src/state/specialised_objects/meta_wcomponents/selecting/derived.ts
+++ app/frontend/src/state/specialised_objects/meta_wcomponents/selecting/derived.ts
@@ -15,16 +15,13 @@ export function derived_meta_wcomponents_state_reducer (initial_state: RootState
 
 function handle_route_changed (initial_state: RootState, state: RootState)
 {
-    // TODO refactor as checking if not wcomponents_edit_multiple seems too complected / brittle.
-    if (initial_state.routing.item_id !== state.routing.item_id && state.routing.sub_route !== "wcomponents_edit_multiple")
+    if (initial_state.routing.item_id !== state.routing.item_id)
     {
-        const selected_wcomponent_ids = new Set<string>()
         if (state.routing.item_id && is_wcomponent_id(state.routing.item_id))
         {
-            selected_wcomponent_ids.add(state.routing.item_id)
+            const selected_wcomponent_ids = new Set([ state.routing.item_id ])
+            state = update_substate(state, "meta_wcomponents", "selected_wcomponent_ids", selected_wcomponent_ids)
         }
-
-        state = update_substate(state, "meta_wcomponents", "selected_wcomponent_ids", selected_wcomponent_ids)
     }
 
     return state
