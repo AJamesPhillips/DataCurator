5858bfd457d0264d932b5ad61d9d46ee000ffc41
pegnott <peg.nottingham@gmail.com>
2021-06-23 15:29:39 -0400

    trim button atts/styles for material ui

--- app/frontend/src/form/editable_list/ListHeaderAddButton.tsx
+++ app/frontend/src/form/editable_list/ListHeaderAddButton.tsx
@@ -1,14 +1,10 @@
 import { h } from "preact"
-
 import { Button } from "../../sharedf/Button"
-
-
 
 interface OwnProps {
     new_item_descriptor: string
     on_pointer_down_new_list_entry: () => void
 }
-
 
 export function ListHeaderAddButton (props: OwnProps)
 {
@@ -17,13 +13,9 @@ export function ListHeaderAddButton (props: OwnProps)
         on_pointer_down_new_list_entry
     } = props
 
-
-    return <Button
-        value={`New ${new_item_descriptor}`}
-        extra_class_names="button_add_new_list_entry"
-        on_pointer_down={e => {
-            e.stopPropagation()
-            on_pointer_down_new_list_entry()
-        }}
-    />
+    return (
+        <Button onClick={on_pointer_down_new_list_entry}>
+            {`New ${new_item_descriptor}`}
+        </Button>
+    )
 }
