4a9bf1ec32f73f093e2f401a25884d2586015dd7
pegnott <peg.nottingham@gmail.com>
2021-06-24 11:25:58 -0400

    material ui on range slider and < > btns

--- app/frontend/src/time_control/TimeSlider.tsx
+++ app/frontend/src/time_control/TimeSlider.tsx
@@ -1,16 +1,14 @@
 import { FunctionalComponent, h } from "preact"
 import { connect, ConnectedProps } from "react-redux"
-
-import "./time_slider.css"
 import { EditableCustomDateTime } from "../form/EditableCustomDateTime"
 import type { RootState } from "../state/State"
 import { find_nearest_index_in_sorted_list } from "../utils/binary_search"
 import type { TimeSliderEvent } from "./interfaces"
 import { NowButton } from "./NowButton"
 import { floor_mseconds_to_resolution } from "../shared/utils/datetime"
-
-
-
+import { Box, ButtonGroup, IconButton, Slider } from "@material-ui/core"
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 interface OwnProps
 {
@@ -21,16 +19,13 @@ interface OwnProps
     title: string
 }
 
-
 const map_state = (state: RootState, { get_handle_ms }: OwnProps) => ({
     handle_datetime_ms: get_handle_ms(state),
     time_resolution: state.display_options.time_resolution,
 })
 
-
 const connector = connect(map_state)
 type Props = ConnectedProps<typeof connector> & OwnProps
-
 
 function _TimeSlider (props: Props)
 {
@@ -43,15 +38,14 @@ function _TimeSlider (props: Props)
 
     const earliest_ms = event_start_datetimes_ms[0]
     const latest_ms = event_start_datetimes_ms[event_start_datetimes_ms.length - 1]
-
     const current_index = find_nearest_index_in_sorted_list(event_start_datetimes_ms, i => i, props.handle_datetime_ms)
 
     function changed_handle_position (e: h.JSX.TargetedEvent<HTMLInputElement, Event>)
     {
+        console.log("ASLKDJLASKJDA")
         const new_handle_datetime_ms = parseInt(e.currentTarget.value)
         props.change_handle_ms(new_handle_datetime_ms)
     }
-
 
     function move_to_event_datetime (direction: 1 | -1)
     {
@@ -77,49 +71,60 @@ function _TimeSlider (props: Props)
         }
     }
 
-
-    return <div className="time_slider" title={props.title}>
-        <div className="slider_container">
-            <input
-                type="button"
-                value="<"
-                disabled={current_index.bounds === "n/a" || current_index.index <= 0}
-                onClick={move_to_event_datetime(-1)}
-            />
-            <input
-                type="button"
-                value=">"
-                disabled={current_index.bounds === "n/a" || current_index.index >= (event_start_datetimes_ms.length - 1)}
-                onClick={move_to_event_datetime(1)}
-            />
-            <input
-                type="range"
-                onChange={changed_handle_position}
-                onMouseUp={changed_handle_position}
-                value={props.handle_datetime_ms}
-                min={earliest_ms}
-                max={latest_ms}
-                list={"tickmarks_timeslider_" + props.data_set_name}
-            />
-        </div>
-        <br />
-
-        <datalist id={"tickmarks_timeslider_" + props.data_set_name}>
-            {event_start_datetimes_ms.map(d => <option value={d}>{d}</option>)}
-        </datalist>
-
-        <div style={{ maxWidth: 200, display: "inline-flex", float: "right" }}>
-            <EditableCustomDateTime
-                value={new Date(props.handle_datetime_ms)}
-                on_change={new_datetime => new_datetime && props.change_handle_ms(new_datetime.getTime())}
-                show_now_shortcut_button={false}
-                show_today_shortcut_button={false}
-                always_allow_editing={true}
-            />
-            <NowButton change_datetime_ms={datetime_ms => props.change_handle_ms(datetime_ms)} />
-        </div>
-
-    </div>
+    const valuetext = (value: number) => `${value}`
+    return (
+        <Box class="time_slider" title="{props.title}" my={2} px={5}>
+            <Box class="slider_container"  display="flex">
+                <ButtonGroup size="small">
+                    <IconButton
+                        onClick={move_to_event_datetime(-1)}
+                        disabled={current_index.bounds === "n/a" || current_index.index <= 0}>
+                        <NavigateBeforeIcon />
+                    </IconButton>
+                    <IconButton
+                        onClick={move_to_event_datetime(1)}
+                        disabled={current_index.bounds === "n/a" || current_index.index >= (event_start_datetimes_ms.length - 1)}>
+                            <NavigateNextIcon />
+                    </IconButton>
+                </ButtonGroup>
+                <Box flexGrow={1}>
+                    <Slider
+                        onChange={changed_handle_position}
+                        color="secondary"
+                        value={props.handle_datetime_ms}
+                        getAriaValueText={valuetext}
+                        step={1}
+                        min={earliest_ms}
+                        max={latest_ms}
+                        valueLabelDisplay="auto"
+                        marks={event_start_datetimes_ms.map(r => { return { value: r, label:'' } })}
+                    />
+                    {/* <br />
+                    <input
+                        type="range"
+                        onChange={changed_handle_position}
+                        value={props.handle_datetime_ms}
+                        min={earliest_ms}
+                        max={latest_ms}
+                        list={"tickmarks_timeslider_" + props.data_set_name}
+                    /> */}
+                </Box>
+            </Box>
+            <datalist id={"tickmarks_timeslider_" + props.data_set_name}>
+                {event_start_datetimes_ms.map(d => <option value={d}>{d}</option>)}
+            </datalist>
+            <Box display="flex" justifyContent="flex-end"alignItems="center">
+                <EditableCustomDateTime
+                    value={new Date(props.handle_datetime_ms)}
+                    on_change={new_datetime => new_datetime && props.change_handle_ms(new_datetime.getTime())}
+                    show_now_shortcut_button={false}
+                    show_today_shortcut_button={false}
+                    always_allow_editing={true}
+                />
+                <NowButton change_datetime_ms={datetime_ms => props.change_handle_ms(datetime_ms)} />
+            </Box>
+        </Box>
+    )
 }
 
 
