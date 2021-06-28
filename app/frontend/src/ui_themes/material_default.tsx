9b26d47f53090203dc672cd13c480f4b4f60ccb6
pegnott <peg.nottingham@gmail.com>
2021-06-28 09:22:44 -0400

    minor theme color adjustments

--- app/frontend/src/ui_themes/material_default.tsx
+++ app/frontend/src/ui_themes/material_default.tsx
@@ -1,55 +1,54 @@
-import { h } from "preact";
 import type { ThemeOptions } from "@material-ui/core/styles";
-import { createMuiTheme }  from '@material-ui/core/styles'
+import { createMuiTheme }    from '@material-ui/core/styles'
 const _default_theme_options: ThemeOptions = {
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 960,
+            lg: 1280,
+            xl: 1920,
+        },
     },
-  },
-  palette: {
-    type: 'light',
+    palette: {
+        type: 'light',
 	primary: {
-      main: '#eceff1',
-    },
-    secondary: {
-      main: '#2196f3',
-    },
-    warning: {
-      main: '#D32F2F',
-    },
+            main: '#e0e0e0',
+        },
+        secondary: {
+            main: '#b3e5fc',
+        },
+        warning: {
+            main: '#f57c00',
+        },
 
-    error: {
-      main: '#B71C1C',
-    },
+        error: {
+            main: '#b71c1c',
+        },
 
-    info: {
-      main: '#2196f3',
-    },
+        info: {
+            main: '#26c6da',
+        },
 
-    success: {
-      main: '#4caf50'
-    }
-  },
-  spacing: 2,
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
+        success: {
+            main: '#7cb342'
+        }
+    },
+    spacing: 2,
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
 };
 
 export const DefaultTheme = createMuiTheme(_default_theme_options)
