1c54c95600f6304cc5f520536749179503556e9e
Alexander James Phillips <ajp.global@gmail.com>
2021-06-28 15:55:55 +0100

    Refactor NewValueAndPredictionSet form

--- app/frontend/src/knowledge/multiple_values/NewValueAndPredictionSet.tsx
+++ app/frontend/src/knowledge/multiple_values/NewValueAndPredictionSet.tsx
@@ -1,8 +1,9 @@
 import { h } from "preact"
+import { useEffect } from "preact/hooks"
 import { useState } from "preact/hooks"
+
 import { uncertain_datetime_is_eternal } from "../../form/datetime_utils"
 import { get_today_date } from "../../shared/utils/date_helpers"
-
 import type { VAPsRepresent } from "../../shared/wcomponent/interfaces/generic_value"
 import type { StateValueAndPredictionsSet } from "../../shared/wcomponent/interfaces/state"
 import { Button } from "../../sharedf/Button"
@@ -14,55 +15,15 @@ export const new_value_and_prediction_set = (VAPs_represent: VAPsRepresent) =>
 {
     // I do not understand why this works as it occurs after a conditional in NewItemForm
     const [show_advanced, set_show_advanced] = useState(false)
+    // useEffect(() => {}, [VAPs_represent])
 
     return (VAP_set: StateValueAndPredictionsSet, on_change: (item: StateValueAndPredictionsSet) => void) =>
 {
 
-    if (!VAPs_represent.boolean)
-    {
-        return <div>
-            {get_summary_for_single_VAP_set(VAPs_represent, false, undefined)(VAP_set, on_change)}
-            {get_details_for_single_VAP_set(VAPs_represent)(VAP_set, on_change)}
-            {get_details2_for_single_VAP_set(VAPs_represent, true)(VAP_set, on_change)}
-        </div>
-    }
-
-
-    const entry = VAP_set.entries.length === 1 && VAP_set.entries[0]
-    const is_true = !!entry && entry.probability === 1 && entry.conviction === 1
-    const is_false = !!entry && entry.probability === 0 && entry.conviction === 1
-
-    const is_eternal = uncertain_datetime_is_eternal(VAP_set.datetime)
-
     return <div>
-        Set to <br />
-        {entry && !is_true && <Button
-            value="Set to True"
-            onClick={() =>
-            {
-                on_change({ ...VAP_set, entries: [{ ...entry, probability: 1, conviction: 1 }] })
-            }}
-        />}
-        {entry && !is_false && <Button
-            value="Set to False"
-            onClick={() => {
-                on_change({ ...VAP_set, entries: [{ ...entry, probability: 0, conviction: 1 }] })
-            }}
-        />}
+        {VAPs_represent.boolean && <SimplifiedBooleanForm VAP_set={VAP_set} on_change={on_change} />}
 
-        <br /><br />
-
-        {entry && is_eternal && <Button
-            value="Set to 'From today'"
-            onClick={() => on_change({ ...VAP_set, datetime: { min: get_today_date() } })}
-        />}
-
-        {entry && !is_eternal && <Button
-            value="Set to Eternal"
-            onClick={() => on_change({ ...VAP_set, datetime: {} })}
-        />}
-
-        <br /><br />
+        {<SimplifiedDatetimeForm VAP_set={VAP_set} on_change={on_change} />}
 
         <Button
             value={(show_advanced ? "Hide" : "Show") + " advanced options"}
@@ -81,4 +42,77 @@ export const new_value_and_prediction_set = (VAPs_represent: VAPsRepresent) =>
 
     </div>
 }
+}
+
+
+
+interface SimplifiedBooleanFormProps
+{
+    VAP_set: StateValueAndPredictionsSet
+    on_change: (VAP_set: StateValueAndPredictionsSet) => void
+}
+function SimplifiedBooleanForm (props: SimplifiedBooleanFormProps)
+{
+    const { VAP_set, on_change } = props
+
+    const entry = VAP_set.entries[0]
+
+    if (!entry) return null
+
+
+    const is_true = !!entry && entry.probability === 1 && entry.conviction === 1
+    const is_false = !!entry && entry.probability === 0 && entry.conviction === 1
+
+
+    return <div>
+        {!is_true && <Button
+            value="Set to True"
+            onClick={() =>
+            {
+                on_change({ ...VAP_set, entries: [{ ...entry, probability: 1, conviction: 1 }] })
+            }}
+        />}
+        {!is_false && <Button
+            value="Set to False"
+            onClick={() => {
+                on_change({ ...VAP_set, entries: [{ ...entry, probability: 0, conviction: 1 }] })
+            }}
+        />}
+
+        <br /><br />
+    </div>
+}
+
+
+
+interface SimplifiedDatetimeFormProps
+{
+    VAP_set: StateValueAndPredictionsSet
+    on_change: (VAP_set: StateValueAndPredictionsSet) => void
+}
+function SimplifiedDatetimeForm (props: SimplifiedDatetimeFormProps)
+{
+    const { VAP_set, on_change } = props
+
+    const entry = VAP_set.entries[0]
+
+    if (!entry) return null
+
+
+    const is_eternal = uncertain_datetime_is_eternal(VAP_set.datetime)
+
+
+    return <div>
+        {is_eternal && <Button
+            value="Set to 'From today'"
+            onClick={() => on_change({ ...VAP_set, datetime: { min: get_today_date() } })}
+        />}
+
+        {!is_eternal && <Button
+            value="Set to Eternal"
+            onClick={() => on_change({ ...VAP_set, datetime: {} })}
+        />}
+
+        <br /><br />
+    </div>
 }
