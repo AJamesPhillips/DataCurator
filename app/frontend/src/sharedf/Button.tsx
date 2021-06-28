bc410a9bc752f807135ad5f78c77881f7e9079d3
pegnott <peg.nottingham@gmail.com>
2021-06-28 12:09:00 -0400

    user <Hidden> mui ele for to hide/show confirm/del

--- app/frontend/src/sharedf/Button.tsx
+++ app/frontend/src/sharedf/Button.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact"
 import { Button as MaterialButton } from "@material-ui/core"
 import type { ButtonProps } from "@material-ui/core/Button"
-
+import { Hidden } from '@material-ui/core';
 
 
 interface SpecificProps
@@ -12,33 +12,34 @@ interface SpecificProps
 
 export function Button (props: ButtonProps & SpecificProps)
 {
-	if (props.is_hidden) return null
+
     return (
-        <MaterialButton
-            title={props.title}
-            classes={props.classes}
-            color={props.color || "primary"}
-            component={props.component}
-            disabled={props.disabled || false}
-            disableElevation={props.disableElevation || true}
-            disableFocusRipple={props.disableFocusRipple || false}
-            endIcon={props.endIcon}
-            fullWidth={props.fullWidth || false}
-            href={props.href}
-            size={props.size || "small"}
-            startIcon={props.startIcon}
-            variant={props.variant || "contained"}
-            onPointerDown={(e: h.JSX.TargetedMouseEvent<HTMLButtonElement>) =>
-            {
-                // Not sure we want to put this here
-                e.stopImmediatePropagation()
-                e.preventDefault()
-                // Using onPointerDown otherwise other onPointerDown on parent elements fire
-                // and the onClick here never gets to fire
-                props.onPointerDown ? props.onPointerDown(e) : (props.onClick && props.onClick(e))
-            }}
-        >
-            { props.children || props.value }
-        </MaterialButton>
+        <Hidden xsUp={props.is_hidden}>
+            <MaterialButton
+                title={props.title}
+                color={props.color || "primary"}
+                component={props.component}
+                disabled={props.disabled || false}
+                disableElevation={props.disableElevation || true}
+                disableFocusRipple={props.disableFocusRipple || false}
+                endIcon={props.endIcon}
+                fullWidth={props.fullWidth || false}
+                href={props.href}
+                size={props.size || "small"}
+                startIcon={props.startIcon}
+                variant={props.variant || "contained"}
+                onPointerDown={(e: h.JSX.TargetedMouseEvent<HTMLButtonElement>) =>
+                {
+                    // Not sure we want to put this here
+                    e.stopImmediatePropagation()
+                    e.preventDefault()
+                    // Using onPointerDown otherwise other onPointerDown on parent elements fire
+                    // and the onClick here never gets to fire
+                    props.onPointerDown ? props.onPointerDown(e) : (props.onClick && props.onClick(e))
+                }}
+            >
+                { props.children || props.value }
+            </MaterialButton>
+        </Hidden>
     )
 }
