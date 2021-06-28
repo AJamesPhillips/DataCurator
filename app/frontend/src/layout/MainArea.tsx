d2811a5a0d7d71780a540c79b9c7dfb3859660b9
pegnott <peg.nottingham@gmail.com>
2021-06-28 16:11:32 -0400

    divs to Box, styleprops, fix overflow

--- app/frontend/src/layout/MainArea.tsx
+++ app/frontend/src/layout/MainArea.tsx
@@ -1,6 +1,7 @@
 import "./MainArea.scss"
 import { h } from "preact"
 import { MainContentControls } from "./MainContentControls"
+import { Box } from "@material-ui/core"
 
 
 
@@ -11,13 +12,13 @@ interface OwnProps {
 export function MainArea (props: OwnProps)
 {
     return (
-        <div id="main_area">
-            <div id="main_content">
+        <Box id="main_area" display="flex" flexDirection="column" flexGrow={1} flexShrink={1}>
+            <Box id="main_content" flexGrow={1} flexShrink={1} display="flex" flexDirection="column">
                 {props.main_content}
-            </div>
-            <div id="main_content_controls">
+            </Box>
+            <Box id="main_content_controls"  flexGrow={0} flexShrink={1} >
                 <MainContentControls />
-            </div>
-        </div>
+            </Box>
+        </Box>
     )
 }
