f49f80961db05b2962ce51cdb1b6a38f842a62fe
Alexander James Phillips <ajp.global@gmail.com>
2021-07-24 14:10:55 +0100

    When selecting KV for component provide KV path as subtitle

--- app/frontend/src/knowledge_view/SelectKnowledgeView.tsx
+++ app/frontend/src/knowledge_view/SelectKnowledgeView.tsx
@@ -18,6 +18,7 @@ interface OwnProps {
 
 const map_state = (state: RootState) => ({
     knowledge_views: state.derived.knowledge_views,
+    nested_knowledge_view_ids_map: state.derived.nested_knowledge_view_ids.map,
 })
 
 const map_dispatch = {
@@ -38,12 +39,24 @@ function _SelectKnowledgeView (props: Props)
         exclude_ids = new Set(),
         on_change,
         knowledge_views,
+        nested_knowledge_view_ids_map,
     } = props
 
 
     const options: AutocompleteOption[] = knowledge_views
         .filter(({ id }) => !exclude_ids.has(id))
-        .map(({ id, title }) => ({ id, title }))
+        .map(({ id, title }) =>
+        {
+            let subtitle = ""
+            let entry = nested_knowledge_view_ids_map[id]
+            while (entry)
+            {
+                subtitle = " / " + entry.title + subtitle
+                entry = nested_knowledge_view_ids_map[entry?.parent_id || ""]
+            }
+
+            return { id, title, subtitle }
+        })
         .sort((kv1, kv2) => kv1.title < kv2.title ? -1 : 1)
 
 
