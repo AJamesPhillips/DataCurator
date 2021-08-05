67058acde811853737376725ca740dffb4700cc4
Alexander James Phillips <ajp.global@gmail.com>
2021-08-05 20:48:25 +0100

    Move CNAME to build project

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
-cd ../data-curator-build/
-git rm -rf .
-git clean -fxd
-cd -
+# set -e
+# cd ../data-curator-build/
+
+# set +e
+# git rm -rf .
+# set -e
+
+# git clean -fxd
+# cd -
 
 cd app/frontend
-npm run build
-mkdir ../../../data-curator-build/app
-cp ../../../data-curator-build/index.html ../../../data-curator-build/app/index.html
+# npm run build
+# mkdir ../../../data-curator-build/app
+# cp ../../../data-curator-build/index.html ../../../data-curator-build/app/index.html
+
+echo "datacurator.org" > ../../../data-curator-build/CNAME
+
 cd -
