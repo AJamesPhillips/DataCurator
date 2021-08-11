e22e17bebdf7d50bed474b58c56019277cb562f8
Alexander James Phillips <ajp.global@gmail.com>
2021-08-11 18:40:43 +0100

    Refactor ConfirmatoryDeleteButton into two parts #93

---
+++ app/frontend/src/form/ConfirmatoryButton.tsx
@@ -0,0 +1,67 @@
+import { Box, ThemeProvider, Tooltip } from "@material-ui/core"
+import { h } from "preact"
+import { useState } from "preact/hooks"
+
+import { DefaultTheme, WarningTheme } from "../ui_themes/material_default"
+import { Button } from "../sharedf/Button"
+
+
+
+interface OwnProps
+{
+    on_click?: () => void,
+    button_text: string
+    button_icon?: h.JSX.Element
+    tooltip_text?: string
+}
+
+
+
+export function ConfirmatoryButton (props: OwnProps)
+{
+    const [progressing, set_progressing] = useState(false)
+    const { tooltip_text = "" } = props
+
+    return (
+        <Box display="flex" justifyContent="space-between" mb={3}>
+            <ThemeProvider theme={WarningTheme}>
+                <Button
+                    color="secondary"
+                    is_hidden={!progressing}
+                    onClick={e =>
+                    {
+                        e.stopImmediatePropagation()
+                        set_progressing(false)
+                        props.on_click && props.on_click()
+                    }}
+                    startIcon={props.button_icon}
+                >
+                    <Tooltip title={tooltip_text} aria-label={tooltip_text}>
+                        <Box component="span">
+                            CONFIRM
+                        </Box>
+                    </Tooltip>
+                </Button>
+            </ThemeProvider>
+            <ThemeProvider theme={DefaultTheme}>
+                <Button
+                    color="primary"
+                    fullWidth={!progressing}
+                    is_hidden={!props.on_click}
+                    onClick={e =>
+                    {
+                        e.stopImmediatePropagation()
+                        set_progressing(!progressing)
+                    }}
+                    startIcon={progressing ? "" : props.button_icon }
+                >
+                    <Tooltip title={progressing ? "" : tooltip_text } aria-label={progressing ? "" : tooltip_text }>
+                        <Box component="span">
+                            {progressing ? "Cancel" : props.button_text}
+                        </Box>
+                    </Tooltip>
+                </Button>
+            </ThemeProvider>
+        </Box>
+    )
+}
--- app/frontend/src/form/ConfirmatoryDeleteButton.tsx
+++ app/frontend/src/form/ConfirmatoryDeleteButton.tsx
@@ -1,13 +1,10 @@
-import { Box, ThemeProvider, Typography } from "@material-ui/core"
 import { FunctionalComponent, h } from "preact"
-
-import { useState } from "preact/hooks"
 import { connect, ConnectedProps } from "react-redux"
-import { DefaultTheme, WarningTheme } from "../ui_themes/material_default"
-import { Button } from "../sharedf/Button"
+import DeleteIcon from "@material-ui/icons/Delete"
+
 import type { RootState } from "../state/State"
-import { Tooltip } from "@material-ui/core"
-import DeleteIcon from '@material-ui/icons/Delete';
+import { ConfirmatoryButton } from "./ConfirmatoryButton"
+
 
 
 interface OwnProps
@@ -16,7 +13,6 @@ interface OwnProps
     button_text?: string
     tooltip_text?: string
 }
-
 
 
 const map_state = (state: RootState) => ({
@@ -29,47 +25,14 @@ type Props = ConnectedProps<typeof connector> & OwnProps
 
 function _ConfirmatoryDeleteButton (props: Props)
 {
-    const [deleting, set_deleting] = useState(false)
-    const toolip_text = props.tooltip_text || ""
     if (props.consumption_formatting) return null
 
-    return (
-		<Box display="flex" justifyContent="space-between" mb={3}>
-			<ThemeProvider theme={WarningTheme}>
-				<Button
-                    color="secondary"
-                    is_hidden={!deleting}
-					onClick={() =>
-					{
-						set_deleting(false)
-						props.on_delete && props.on_delete()
-					}}
-                    startIcon={<DeleteIcon />}
-				>
-                    <Tooltip title={toolip_text} aria-label={toolip_text}>
-                        <Box component="span">
-                            CONFIRM
-                        </Box>
-                    </Tooltip>
-                </Button>
-			</ThemeProvider>
-            <ThemeProvider theme={DefaultTheme}>
-                <Button
-                    color="primary"
-                    fullWidth={!deleting}
-                    is_hidden={!props.on_delete}
-                    onClick={() => set_deleting(!deleting) }
-                    startIcon={deleting ? "" : <DeleteIcon />  }
-                >
-                    <Tooltip title={deleting ? ""  : toolip_text } aria-label={deleting ? ""  : toolip_text }>
-                        <Box component="span">
-                            {deleting ? "Cancel" : (props.button_text || "Delete") }
-                        </Box>
-                    </Tooltip>
-                </Button>
-            </ThemeProvider>
-		</Box>
-	)
+    return <ConfirmatoryButton
+        on_click={props.on_delete}
+        button_text={props.button_text || "Delete"}
+        button_icon={<DeleteIcon />}
+        tooltip_text={props.tooltip_text}
+    />
 }
 
 export const ConfirmatoryDeleteButton = connector(_ConfirmatoryDeleteButton) as FunctionalComponent<OwnProps>
