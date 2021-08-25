9784b634d7b3b2421272e8a5b1595fd0850bcc26
pegnott <peg.nottingham@gmail.com>
2021-08-25 12:39:48 -0400

    update_search_type/is_update_search_type action

--- app/frontend/src/state/search/actions_reducer.ts
+++ app/frontend/src/state/search/actions_reducer.ts
@@ -2,32 +2,37 @@ import type { Action, AnyAction } from "redux"
 
 import { update_substate } from "../../utils/update_state"
 import type { RootState } from "../State"
-import type { SearchFields } from "./state"
-
-
+import type { SearchFields, SearchType } from "./state"
 
 export const search_reducer = (state: RootState, action: AnyAction): RootState =>
 {
-
     if (is_update_search_fields(action))
     {
         state = update_substate(state, "search", "search_fields", action.search_fields)
     }
 
-
+    if (is_update_search_type(action))
+    {
+        state = update_substate(state, "search", "search_type", action.search_type)
+    }
     return state
 }
-
-
 
 interface UpdateSearchFieldsArgs
 {
     search_fields: SearchFields
 }
 
+interface UpdateSearchTypeArgs
+{
+    search_type: SearchType
+}
+
 interface ActionUpdateSearchFields extends Action, UpdateSearchFieldsArgs {}
+interface ActionUpdateSearchType extends Action, UpdateSearchTypeArgs {}
 
 const update_search_fields_type = "update_search_fields"
+const update_search_type_type = "update_search_type"
 
 export const update_search_fields = (args: UpdateSearchFieldsArgs): ActionUpdateSearchFields =>
 {
@@ -38,8 +43,16 @@ const is_update_search_fields = (action: AnyAction): action is ActionUpdateSearc
     return action.type === update_search_fields_type
 }
 
+export const update_search_type = (args: UpdateSearchTypeArgs): ActionUpdateSearchType =>
+{
+    return { type: update_search_fields_type, ...args }
+}
 
+const is_update_search_type = (action: AnyAction): action is ActionUpdateSearchType => {
+    return action.type === update_search_type_type
+}
 
 export const search_actions = {
     update_search_fields,
+    update_search_type,
 }
