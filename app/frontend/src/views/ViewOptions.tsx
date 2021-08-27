aa9bbd8e889dcfe67a74bf2839ab2e629727217e
pegnott <peg.nottingham@gmail.com>
2021-08-27 09:37:27 -0400

    toogleButtonGroup to buttonGroup

--- app/frontend/src/views/ViewOptions.tsx
+++ app/frontend/src/views/ViewOptions.tsx
@@ -1,15 +1,12 @@
 import { FunctionalComponent, h } from "preact"
 import { connect, ConnectedProps } from "react-redux"
-import ToggleButton from "@material-ui/lab/ToggleButton"
-import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup"
-import EditIcon from "@material-ui/icons/Edit"
-import PresentToAllIcon from "@material-ui/icons/PresentToAll"
 
 import { ACTIONS } from "../state/actions"
 import type { RootState } from "../state/State"
-import { Box } from "@material-ui/core"
-
-
+import { Button, ButtonGroup, makeStyles, Typography } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit"
+import PresentToAllIcon from "@material-ui/icons/PresentToAll"
+import { grey, teal, yellow } from "@material-ui/core/colors";
 
 const map_state = (state: RootState) =>
 {
@@ -22,28 +19,41 @@ const map_dispatch = {
     toggle_consumption_formatting: ACTIONS.display.toggle_consumption_formatting,
     change_route: ACTIONS.routing.change_route,
 }
-
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: grey[400],
+        color:grey[600]
+    },
+}));
 const connector = connect(map_state, map_dispatch)
 type Props = ConnectedProps<typeof connector>
-
-
-
 function _ViewOptions (props: Props)
 {
-    return <ToggleButtonGroup
-        size="small"
-        exclusive
-        onChange={props.toggle_consumption_formatting}
-        value={props.presenting ? "presenting" : "editing"}
-        aria-label="text formatting"
-    >
-        <ToggleButton value="editing" aria-label="Editing">
-            <EditIcon />
-        </ToggleButton>
-        <ToggleButton value="presenting" aria-label="Presenting">
-            <PresentToAllIcon />
-        </ToggleButton>
-    </ToggleButtonGroup>
+    const classes = useStyles();
+    return (
+        <ButtonGroup size="small"
+            disableElevation
+            variant="contained"
+            value={props.presenting ? "presenting" : "editing"}
+        >
+            <Button
+                className={classes.root}
+                value="editing"
+                onClick={props.toggle_consumption_formatting}
+                disabled={(!props.presenting) ? true : false }
+            >
+                <EditIcon />
+            </Button>
+            <Button
+                className={classes.root}
+                value="presenting"
+                onClick={props.toggle_consumption_formatting}
+                disabled={(props.presenting) ? true : false }
+            >
+                <PresentToAllIcon />
+            </Button>
+        </ButtonGroup>
+    )
 }
 
 export const ViewOptions = connector(_ViewOptions) as FunctionalComponent<{}>
