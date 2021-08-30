b366c0d7231e6b30225d047651c88811d25e3d64
pegnott <peg.nottingham@gmail.com>
2021-08-30 10:58:21 -0400

    text input -> Material TextField component

--- app/frontend/src/form/editable_text/EditableTextSingleLine.tsx
+++ app/frontend/src/form/editable_text/EditableTextSingleLine.tsx
@@ -1,3 +1,4 @@
+import { TextField } from "@material-ui/core"
 import { h } from "preact"
 
 import {
@@ -12,21 +13,30 @@ export function EditableTextSingleLine (props: EditableTextCommonOwnProps)
 {
     return <EditableTextCommon
         {...props}
-
         component={({ value, on_render, on_focus, on_change, on_blur }: EditableTextComponentArgs) =>
-            <input
-                type="text"
-                placeholder={props.placeholder}
+            <TextField
+                fullWidth={true}
+                label={props.placeholder}
+                variant="outlined"
                 value={value}
-                ref={el =>
-                {
-                    if (!el) return
-                    on_render(el)
-                }}
                 onFocus={on_focus}
                 onChange={on_change}
                 onBlur={on_blur}
             />
+            // @TODO: Check if ref is still needed and convert to Material syntax if so.
+            // <input
+            //     type="text"
+            //     placeholder={props.placeholder}
+            //     value={value}
+            //     ref={el =>
+            //     {
+            //         if (!el) return
+            //         on_render(el)
+            //     }}
+            //     onFocus={on_focus}
+            //     onChange={on_change}
+            //     onBlur={on_blur}
+            // />
         }
     />
 }
