1a790682887430b38a32b09d991bdd4bcf72d040
pegnott <peg.nottingham@gmail.com>
2021-08-31 20:53:50 -0400

    better responsiveness

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -18,6 +18,10 @@ function App()
     const useStyles = makeStyles(theme => ({
         toolbar: {
             justifyContent:"space-between",
+            flexWrap:"wrap",
+            [theme.breakpoints.up('md')]: {
+                flexWrap:"nowrap"
+            }
         },
         toolbar_section: {
             display:"inherit",
@@ -25,14 +29,28 @@ function App()
             flexShrink:1,
             flexBasis:"auto",
             marginRight:5,
-            "&:last-child": { marginRight:0 }
+            "&:last-child": { marginRight:0 },
+            "&:empty": { display:"none" }
         },
         toolbar_item: {
             marginRight:5,
             "&:last-child": { marginRight:0 }
+
+
         },
         grow: {
             flexGrow:1,
+        },
+        small_full_width: {
+            [theme.breakpoints.down('sm')]: {
+                flexGrow:0, flexShrink:1, flexBasis:"100%",
+                margin: 0,
+                // justifyContent:"flex-end",
+                // "& > *:last-child": {
+                //     border:"1px purple solid",
+                //     // flexGrow:1,
+                // },
+            }
         }
 
       }));
@@ -44,16 +62,19 @@ function App()
                 <Box component="header" zIndex={100}>
                     <AppBar position="static">
                         <Toolbar variant="dense" className={classes.toolbar}>
-                            <Box className={`${classes.toolbar_section}`}>
-                                <ViewOptions />
-                            </Box>
-                            <Box className={`${classes.toolbar_section} ${classes.grow}`}>
-                                <ViewsBreadcrumb />
-                            </Box>
-                            <Box className={`${classes.toolbar_section}`}>
+                            <Box className={`${classes.toolbar_section} ${classes.grow} ${classes.small_full_width}`}>
                                 <Box className={`${classes.toolbar_item}`}>
-                                    <BackupInfo />
+                                    <ViewOptions />
                                 </Box>
+                                <Box className={`${classes.toolbar_item} ${classes.grow}`}>
+                                    <ViewsBreadcrumb />
+                                </Box>
+                            </Box>
+                            {/* <Box className={`${classes.toolbar_section} ${classes.grow}`} justifyContent="space-between">
+
+                            </Box> */}
+                            <Box className={`${classes.toolbar_section} ${classes.small_full_width}`} justifyContent="flex-end">
+                                <Box className={`${classes.toolbar_item}`}><BackupInfo /></Box>
                                 <Box className={`${classes.toolbar_item}`}>
                                     <SyncInfo />
                                 </Box>
