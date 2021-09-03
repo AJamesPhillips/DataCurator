ea245371518231bfe5db2ac352ad92b17f211bef
pegnott <peg.nottingham@gmail.com>
2021-09-03 10:54:30 -0400

    ui for typing/pasting date

--- app/frontend/src/form/MaterialDatetime.tsx
+++ app/frontend/src/form/MaterialDatetime.tsx
@@ -1,12 +1,11 @@
-import { FunctionalComponent, h, Ref } from "preact"
-
-import "./Editable.css"
+import { FunctionalComponent, h  } from "preact"
 import { useState } from "preact/hooks"
-import { date2str, get_today_str } from "../shared/utils/date_helpers"
+import { date2str } from "../shared/utils/date_helpers"
 import { connect, ConnectedProps } from "react-redux"
 import type { RootState } from "../state/State"
-import { Box, IconButton, TextField } from "@material-ui/core"
+import { Box, InputAdornment, makeStyles, Popover, TextField } from "@material-ui/core"
 import FileCopyIcon from '@material-ui/icons/FileCopy';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 interface OwnProps
 {
@@ -14,6 +13,8 @@ interface OwnProps
     value?: Date | undefined
     on_change?: (new_datetime: Date | undefined) => void
     always_allow_editing?: boolean
+    fullWidth?: boolean
+    required?: boolean
 }
 
 const map_state = (state: RootState) => ({
@@ -26,23 +27,88 @@ type Props = ConnectedProps<typeof connector> & OwnProps
 
 function _MaterialDateTime (props: Props)
 {
-    const not_editable = props.always_allow_editing ? false : props.presenting
+    const [popover_anchor, set_popover_anchor] = useState(null);
+    const [is_valid_date, set_is_valid_date] = useState(true);
+    const [the_date, set_the_date] = useState(props.value);
+
+    const handle_manual_date_click = (e:any) => {
+        set_popover_anchor(e.target)
+    }
+    const handle_close_paste_popover = () => {
+        set_popover_anchor(null)
+    }
+
+    const not_editable = props.always_allow_editing ? false : props.presenting;
+    const is_popover_open:boolean = Boolean(popover_anchor);
+
+    const on_popover_value_changed = (event:any) => {
+        let input_element:HTMLInputElement = event.target;
+        let maybe_date = new Date(input_element.value);
+        if (isNaN(maybe_date.getTime())) {
+            console.error(`${input_element.value} is not a valid date or datetime value!`);
+            set_is_valid_date(false);
+        } else {
+            set_is_valid_date(true);
+            set_the_date(maybe_date);
+            handle_close_paste_popover();
+        }
+    }
+    const useStyles = makeStyles(theme => ({
+        open_popover_icon: {
+            cursor: "pointer",
+        }
+    }))
+    const classes = useStyles()
     return (
-        <Box>
+        <Box display="flex" justifyContent="space-between">
             <TextField
-                label={props.title || null}
-                value={(props?.value) ? date2str(props.value, "yyyy-MM-ddThh:mm") : null}
                 disabled={not_editable}
-                size="small"
-                type="datetime-local"
-                variant="outlined"
+                fullWidth={(props.fullWidth || false)}
+                InputProps={{
+                    endAdornment: (
+                        <FileCopyIcon
+                            className={classes.open_popover_icon}
+                            onClick={handle_manual_date_click}
+                    />)
+                }}
+                label={props.title || null}
                 onChange={(e:any) => {
                     if (props.on_change) props.on_change(new Date(e.target.value))
                 }}
+                required={props.required || false}
+                size="small"
+                type="datetime-local"
+                value={(the_date) ? date2str(the_date, "yyyy-MM-ddThh:mm") : null}
+                variant="outlined"
             />
-            {/* <IconButton>
-                <FileCopyIcon />
-            </IconButton> */}
+            <Popover
+                open={is_popover_open}
+                onClose={handle_close_paste_popover}
+                anchorEl={popover_anchor}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <Box p={10}>
+                    <TextField
+                        error={!is_valid_date}
+                        helperText="Manually type or paste a date or date/time. Many formats are accepted."
+                        InputProps={{
+                            endAdornment: (<CalendarTodayIcon />)
+                        }}
+                        onChange={on_popover_value_changed}
+                        label="Date / DateTime"
+                        size="small"
+                        type="text"
+                        variant="outlined"
+                    />
+                </Box>
+            </Popover>
         </Box>
     )
 }
