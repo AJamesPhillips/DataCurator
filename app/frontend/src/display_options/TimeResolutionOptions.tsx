d7e5ec260e7af7ea78dd793ddefc4a3009fb35cf
pegnott <peg.nottingham@gmail.com>
2021-09-03 14:34:33 -0400

    inverse appearance of disabled

--- app/frontend/src/display_options/TimeResolutionOptions.tsx
+++ app/frontend/src/display_options/TimeResolutionOptions.tsx
@@ -1,18 +1,15 @@
-import { Box, Button, ButtonGroup, makeStyles } from "@material-ui/core"
-import { grey } from "@material-ui/core/colors"
-import { ToggleButton, ToggleButtonGroup } from "@material-ui/lab"
+import { Button, ButtonGroup } from "@material-ui/core"
 import { FunctionalComponent, h } from "preact"
 import { connect, ConnectedProps } from "react-redux"
 import { ACTIONS } from "../state/actions"
 import { time_resolution_types } from "../state/display_options/state"
 import type { RootState } from "../state/State"
-
+import { invertDisabledAppearance } from "../ui_themes/invert_disabled"
 
 interface OwnProps
 {
     extra_styles?: h.JSX.CSSProperties
 }
-
 
 const map_state = (state: RootState) => ({
     time_resolution: state.display_options.time_resolution,
@@ -28,24 +25,27 @@ type Props = ConnectedProps<typeof connector> & OwnProps
 
 function _TimeResolutionOptions (props: Props)
 {
-    return (!props.display_by_simulated_time) && <ButtonGroup
-        disableElevation
-        variant="contained"
-        value={props.time_resolution}
-    >
-        {time_resolution_types.map(type => (
-            <Button
-                value={type}
-                onClick={(e:any) => {
-                    const time_resolution:any = e.currentTarget.value
-                    time_resolution && props.set_time_resolution({ time_resolution })
-                }}
-                disabled={(props.time_resolution == type) ? true : false }
-            >
-                {type}
-            </Button>)
-        )}
-    </ButtonGroup>
+    const classes = invertDisabledAppearance();
+    return (!props.display_by_simulated_time) &&
+        <ButtonGroup
+            disableElevation
+            variant="contained"
+            value={props.time_resolution}
+        >
+            {time_resolution_types.map(type => (
+                <Button
+                    value={type}
+                    onClick={(e:any) => {
+                        const time_resolution:any = e.currentTarget.value
+                        time_resolution && props.set_time_resolution({ time_resolution })
+                    }}
+                    className={classes.inverse_disabled}
+                    disabled={(props.time_resolution == type) ? true : false }
+                >
+                    {type}
+                </Button>)
+            )}
+        </ButtonGroup>
 }
 
 export const TimeResolutionOptions = connector(_TimeResolutionOptions) as FunctionalComponent<OwnProps>
