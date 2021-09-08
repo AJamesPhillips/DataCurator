bd60af8fe20a2e62070c71bfc41f7c8c2d99b1d3
pegnott <peg.nottingham@gmail.com>
2021-09-08 06:35:04 -0400

    save summary_image string

--- app/frontend/src/knowledge/wcomponent_form/WComponentImageForm.tsx
+++ app/frontend/src/knowledge/wcomponent_form/WComponentImageForm.tsx
@@ -1,21 +1,41 @@
-import { Box, FormControl, FormControlLabel } from "@material-ui/core"
-import { h } from "preact"
-import { DragDropUploader } from "../../form/DragDropUploader"
+import { Box, FormControl, FormControlLabel, TextField } from "@material-ui/core"
+import { FunctionalComponent, h } from "preact"
+import { connect, ConnectedProps } from "react-redux"
+import type { WComponent } from "src/shared/wcomponent/interfaces/SpecialisedObjects"
+import type { RootState } from "src/state/State"
 interface OwnProps
 {
-
+    wcomponent: WComponent
+    upsert_wcomponent: (partial_wcomponent: Partial<WComponent>) => void
 }
 
+const map_state = (state: RootState) => ({
+    creation_context_state: state.creation_context,
+})
 
-export function WComponentImageForm(props: OwnProps)
+const connector = connect(map_state)
+type Props = ConnectedProps<typeof connector> & OwnProps
+
+export function _WComponentImageForm(props: Props)
 {
+    const { wcomponent, upsert_wcomponent } = props;
+    const summary_image = wcomponent.summary_image || undefined;
 
+    // http://upload.wikimedia.org/wikipedia/commons/e/ec/Short-horned_chameleon_%28Calumma_brevicorne%29_female_Andasibe.jpg
     return (
-        <Box height={100} border={1} id="HIALSKDJALSKJDLAKSJD">
-            <DragDropUploader
-                label="Upload a cover image: "
-                valid_file_types={['image/jpeg', 'image/jpg', 'image/png', 'image/gif']}
-            />
-        </Box>
+        <TextField
+            fullWidth={true}
+            label="Summary Image URL"
+            onChange={(e:any) => {
+                let url = (e.target?.value) ? e.target?.value : null
+                if (url) {
+                    upsert_wcomponent({ summary_image: url })
+                }
+            }}
+            value={summary_image}
+            variant="outlined"
+        />
     )
 }
+
+export const WComponentImageForm = connector(_WComponentImageForm) as FunctionalComponent<OwnProps>
