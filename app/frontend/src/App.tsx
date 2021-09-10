5def5eb0fa625d6d5b8c72a2fea13e24080b556e
pegnott <peg.nottingham@gmail.com>
2021-09-10 09:08:50 -0400

    Collapsable side panel Fixes #134

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -18,81 +18,70 @@ import { HelpMenu } from "./help_menu/HelpMenu"
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
 
-
 function App()
 {
     const drawerWidth = 340;
     const useStyles = makeStyles(theme => ({
-        app: {
-            flexGrow:1,
-            flexShrink:1,
+        root: {
+            width:"100%", height:"100%",
             overflow: "hidden",
-            display: "flex", flexDirection: "column", flexWrap: "nowrap",
+            display: "flex",
         },
 
         app_bar: {
-            flexGrow:0,
-            flexShrink:0,
-            position:"relative",
-            zIndex: 100,
-            transition: theme.transitions.create(['margin', 'width'], {
+            marginRight:0,
+            width:"100%", maxWidth:"100%", minWidth:0,
+            transition: theme.transitions.create(['all'], {
                 easing: theme.transitions.easing.sharp,
                 duration: theme.transitions.duration.leavingScreen,
             }),
+            zIndex: theme.zIndex.drawer + 1,
         },
 
         app_bar_with_open_side_panel: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            transition: theme.transitions.create(['margin', 'width'], {
+            width: `calc(100% - ${drawerWidth}px)`, maxWidth:`calc(100% - ${drawerWidth}px)`,
+            marginRight: drawerWidth,
+            transition: theme.transitions.create(['all'], {
                 easing: theme.transitions.easing.easeOut,
                 duration: theme.transitions.duration.enteringScreen,
             }),
-            marginRight: drawerWidth,
         },
-
-        main : {
-            position:"relative",
-            zIndex: 1,
-            flexGrow:1, flexShrink:1,
-            display:"flex", flexDirection:"row", flexWrap: "nowrap",
-        },
-
         content: {
-            height:"100%",
-            flexGrow:1, flexShrink:1,
+            position:"relative", zIndex: 1,
+            flexGrow:1,
+            flexShrink:1,
             display:"flex", flexDirection:"column", flexWrap: "nowrap",
-            transition: theme.transitions.create('margin', {
+            marginRight: -drawerWidth,
+            transition: theme.transitions.create(['margin'], {
                 easing: theme.transitions.easing.sharp,
                 duration: theme.transitions.duration.leavingScreen,
             }),
-            marginRight: -drawerWidth,
         },
 
         content_with_open_side_panel: {
-            transition: theme.transitions.create('margin', {
+            marginRight: 0,
+            transition: theme.transitions.create(['margin'], {
                 easing: theme.transitions.easing.easeOut,
                 duration: theme.transitions.duration.enteringScreen,
             }),
-            marginRight: 0,
         },
 
         drawer: {
             width: drawerWidth,
             flexShrink: 0,
-            overflow:"hidden",
         },
 
         side_panel: {
             backgroundColor:theme.palette.background.paper,
             width: drawerWidth,
-            position:"relative", zIndex: 1000,
         },
 
         side_panel_content: {
-            // marginTop:1, padding:5,
+            marginTop:10, padding:10,
         },
 
         toolbar: {
+            flexGrow: 1,
             justifyContent:"space-between",
             flexWrap:"wrap",
             [theme.breakpoints.up('md')]: {
@@ -122,16 +111,16 @@ function App()
         hide: { display: 'none' },
       }));
 
-      const classes = useStyles();
+    const classes = useStyles();
     const [side_panel_open, set_side_panel_open] = useState(true);
-    const handle_open_side_panel = () => set_side_panel_open(true);
-    const handle_close_side_panel = () => set_side_panel_open(false);
+    const handle_open_side_panel = () => { set_side_panel_open(true); console.log("open"); }
+    const handle_close_side_panel = () => { set_side_panel_open(false); console.log("closed"); }
 
     return (
         <ThemeProvider theme={DefaultTheme}>
             <CssBaseline />
-            <Box id="app" className={classes.app}>
-                <AppBar position="static" className={clsx(classes.app_bar, { [classes.app_bar_with_open_side_panel]: side_panel_open })}>
+            <Box id="app" className={classes.root}>
+                <AppBar elevation={1} id="header" position="fixed" className={clsx(classes.app_bar, { [classes.app_bar_with_open_side_panel]: side_panel_open })}>
                     <Toolbar variant="dense" className={classes.toolbar}>
                         <Box className={`${classes.toolbar_section} ${classes.grow} ${classes.small_full_width}`}>
                             <Box className={`${classes.toolbar_item}`}>
@@ -143,16 +132,9 @@ function App()
                         </Box>
                         <Box className={`${classes.toolbar_section} ${classes.small_full_width}`} justifyContent="flex-end">
                             <Box className={`${classes.toolbar_item}`}><BackupInfo /></Box>
-                            <Box className={`${classes.toolbar_item}`}>
-                                <SyncInfo />
-                            </Box>
-                            <Box className={`${classes.toolbar_item}`}>
-                                <StorageInfo />
-                            </Box>
-                            <Box className={`${classes.toolbar_item}`}>
-                                <UserInfo />
-                            </Box>
-
+                            <Box className={`${classes.toolbar_item}`}><SyncInfo /></Box>
+                            <Box className={`${classes.toolbar_item}`}><StorageInfo /></Box>
+                            <Box className={`${classes.toolbar_item}`}><UserInfo /></Box>
                             <Box className={`${classes.toolbar_item}`}>
                                 <IconButton
                                     aria-label="open side panel"
@@ -168,43 +150,35 @@ function App()
                         </Box>
                     </Toolbar>
                 </AppBar>
-                <Box component="main"  className={`${classes.main} ${(side_panel_open) ? 'side_panel_open' : 'side_panel_closed' }`}>
-                    <Box id="app_content" className={clsx(classes.content, { [classes.content_with_open_side_panel]: side_panel_open, })}>
-                        <MainAreaRouter />
-                    </Box>
-                    <Drawer
-                        anchor="right"
-                        className={classes.drawer}
-                        open={side_panel_open}
-                        variant="persistent"
-                    >
-                        <Box component="aside" className={classes.side_panel} id="side_panel">
-                            <AppBar  position="sticky">
-                                <Toolbar variant="dense">
-                                    <IconButton
-                                        color="inherit"
-                                        aria-label="Close side panel"
-                                        edge="end"
-                                        onClick={handle_close_side_panel}
-                                    >
-                                        <CloseIcon />
-                                    </IconButton>
-                                </Toolbar>
-                            </AppBar>
-                            {/* <AppBar  position="sticky" className={classes.drawer_header}>
-                                <Toolbar variant="dense">
-
-                                </Toolbar>
-                            </AppBar> */}
-                            <Box id="side_panel_content" classNames={classes.side_panel_content}>
-                                <AppMenuItemsContainer />
-                                <SidePanel />
-                            </Box>
-                        </Box>
-                    </Drawer>
-                    <HelpMenu />
+                <Box id="app_content" component="main" className={clsx(classes.content, { [classes.content_with_open_side_panel]: side_panel_open, })}>
+                    <MainAreaRouter />
                 </Box>
-                <Box component="footer"></Box>
+                <Drawer
+                    anchor="right"
+                    className={classes.drawer}
+                    open={side_panel_open}
+                    variant="persistent"
+                >
+                    <Box component="aside" className={classes.side_panel} id="side_panel">
+                        <AppBar elevation={1}  position="sticky" >
+                            <Toolbar variant="dense">
+                                <IconButton
+                                    color="inherit"
+                                    aria-label="Close side panel"
+                                    edge="end"
+                                    onClick={handle_close_side_panel}
+                                >
+                                    <CloseIcon />
+                                </IconButton>
+                            </Toolbar>
+                        </AppBar>
+                        <Box id="side_panel_content" className={classes.side_panel_content}>
+                            <AppMenuItemsContainer />
+                            <SidePanel />
+                        </Box>
+                    </Box>
+                </Drawer>
+                <HelpMenu />
             </Box>
         </ThemeProvider>
     )
