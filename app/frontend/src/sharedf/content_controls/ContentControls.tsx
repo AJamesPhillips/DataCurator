0980ee4b0403d4eaf5aa47b84dc32f9267401dda
pegnott <peg.nottingham@gmail.com>
2021-09-14 10:24:40 -0400

    add warning if no nodes are visible due via filter

--- app/frontend/src/sharedf/content_controls/ContentControls.tsx
+++ app/frontend/src/sharedf/content_controls/ContentControls.tsx
@@ -1,7 +1,7 @@
 import { FunctionalComponent, h } from "preact"
 import { connect, ConnectedProps } from "react-redux"
 
-import { Box, ButtonGroup, Button } from "@material-ui/core"
+import { Box, ButtonGroup, Button, Toolbar, makeStyles, Tooltip } from "@material-ui/core"
 import { MoveToWComponentButton } from "../../canvas/MoveToWComponentButton"
 import { TimeResolutionOptions } from "../../display_options/TimeResolutionOptions"
 import { ACTIONS } from "../../state/actions"
@@ -9,7 +9,8 @@ import type { RootState } from "../../state/State"
 import { TimeSlider } from "../../time_control/TimeSlider"
 import type { TimeSliderEvent } from "../../time_control/interfaces"
 import { invertDisabledAppearance } from "../../ui_themes/invert_disabled"
-
+import { Alert } from "@material-ui/lab"
+import WarningIcon from '@material-ui/icons/Warning';
 
 interface OwnProps
 {
@@ -18,12 +19,12 @@ interface OwnProps
     sim_events: TimeSliderEvent[]
 }
 
-
 const map_state = (state: RootState) => ({
     linked_datetime_sliders: state.controls.linked_datetime_sliders,
     display_by_simulated_time: state.display_options.display_by_simulated_time,
     display_time_sliders: state.controls.display_time_sliders,
     editing: !state.display_options.consumption_formatting,
+    created_at_ms: state.routing.args.created_at_ms,
 })
 
 const map_dispatch = {
@@ -37,27 +38,47 @@ const map_dispatch = {
 const connector = connect(map_state, map_dispatch)
 type Props = ConnectedProps<typeof connector> & OwnProps
 
-
 function _ContentControls (props: Props)
 {
-    const classes = invertDisabledAppearance();
+    const intvert_classes = invertDisabledAppearance();
     const { created_events, sim_events, move_to_component_id } = props
     const set_knowledge_view_type = (e: h.JSX.TargetedMouseEvent<HTMLButtonElement>) => {
         const display_by_simulated_time = JSON.parse(e.currentTarget.value);
         props.set_display_by_simulated_time({ display_by_simulated_time });
     }
 
-    const display_sliders = props.editing || props.display_time_sliders
 
+    const display_sliders = props.editing || props.display_time_sliders
+    let are_any_component_dates_within_filter:boolean = false;
+    let how_many_components_are_visible:number = 0;
+
+    created_events.forEach(e => {
+        const filtered_created_at_datetime: Date = new Date(props.created_at_ms);
+        const component_created_date = e.datetime;
+        if (filtered_created_at_datetime.getTime() >= component_created_date.getTime()) {
+            are_any_component_dates_within_filter = true;
+            how_many_components_are_visible++
+        }
+    })
+    const useStyles = makeStyles(theme => ({
+        toolbar: {
+            justifyContent:"space-between"
+        },
+    }))
+    const classes = useStyles()
     return (
         <Box p={2} mb={2} borderTop={1} borderColor="primary.main">
-            {/* <div style={{ width: 40, display: "inline-block" }}></div> */}
-
-            <Box mb={2}  display="flex" flexDirection="row" justifyContent="space-between">
+            <Toolbar className={classes.toolbar} variant="dense">
                 <Box>
                     <MoveToWComponentButton wcomponent_id={move_to_component_id} />
                 </Box>
-
+                {(!are_any_component_dates_within_filter) && (
+                    <Tooltip placement="top" title="WARNING: No components are visible due to create_at filter!">
+                        <Alert severity="warning" icon={<WarningIcon />}>
+                            {how_many_components_are_visible}
+                        </Alert>
+                    </Tooltip>
+                )}
                 <Box component="label" title={props.editing ? "Time sliders always shown whilst editing" : ""}>
                     <Button
                         disabled={props.editing}
@@ -82,7 +103,7 @@ function _ContentControls (props: Props)
                             value={true}
                             onClick={set_knowledge_view_type}
                             aria-label="Display by simulated time"
-                            className={classes.inverse_disabled}
+                            className={intvert_classes.inverse_disabled}
                             disabled={props.display_by_simulated_time}
                         >
                             Time
@@ -91,14 +112,14 @@ function _ContentControls (props: Props)
                             value={false}
                             onClick={set_knowledge_view_type}
                             aria-label="Display by relationships"
-                            className={classes.inverse_disabled}
+                            className={intvert_classes.inverse_disabled}
                             disabled={!props.display_by_simulated_time}
                         >
                             Relationships
                         </Button>
                     </ButtonGroup>
                 </Box>
-            </Box>
+            </Toolbar>
 
             {display_sliders && <Box
                 display="flex" flexDirection="row" alignItems="center" alignContent="center"
