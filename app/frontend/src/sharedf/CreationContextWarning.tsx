d9fa4e4e404f4352354797bf2c0f1b1cf1e840b5
Alexander James Phillips <ajp.global@gmail.com>
2021-09-15 13:57:19 +0100

    Limit showing creation context warning to when editing

--- app/frontend/src/sharedf/CreationContextWarning.tsx
+++ app/frontend/src/sharedf/CreationContextWarning.tsx
@@ -1,28 +1,32 @@
 import { FunctionalComponent, h } from "preact"
 import { IconButton, makeStyles,Tooltip } from "@material-ui/core"
 
-import PhotoFilterIcon from '@material-ui/icons/PhotoFilter';
-import type { RootState } from "../state/State";
-import { connect, ConnectedProps } from "react-redux";
+import PhotoFilterIcon from "@material-ui/icons/PhotoFilter"
+import type { RootState } from "../state/State"
+import { connect, ConnectedProps } from "react-redux"
+
+
 
 interface OwnProps {}
 const map_state = (state: RootState) => ({
     creation_context: state.creation_context,
+    editing: !state.display_options.consumption_formatting,
 })
 
 const connector = connect(map_state)
 type Props = ConnectedProps<typeof connector>  & OwnProps
 
-function _CreationContextWarning(props: Props)
+function _CreationContextWarning (props: Props)
 {
-    const { creation_context } = props
+    const { creation_context, editing } = props
     const useStyles = makeStyles(theme => ({
-        warning_button: { cursor:"help" },
+        warning_button: { cursor: "help" },
         warning_icon: { color: theme.palette.warning.main }
-      }));
-    const classes = useStyles();
-    return (creation_context) && (
-        <Tooltip placement="top" title="WARNING: creation_context is active, which can result in components being created with inadvertently incorrect information!">
+    }))
+    const classes = useStyles()
+
+    return (creation_context.use_creation_context && editing) && (
+        <Tooltip placement="top" title="WARNING: Creation Context is active, which can result in components being created with incorrect information!">
             <IconButton
                 className={classes.warning_button}
                 component="span"
