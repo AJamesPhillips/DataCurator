d081d5d5c7b3108d6c748a8e388491185adacc71
Alexander James Phillips <ajp.global@gmail.com>
2021-09-18 14:38:02 +0100

    Extend solid sandbox with trinpod but Trinpod fails with CORS error

--- app/frontend/src/scratch_pad/SandBoxSolid.tsx
+++ app/frontend/src/scratch_pad/SandBoxSolid.tsx
@@ -8,53 +8,86 @@ import {
     getThingAll,
     getStringNoLocale,
     deleteSolidDataset,
+    getResourceAcl,
+    getSolidDatasetWithAcl,
+    getAgentResourceAccess,
+    getAgentAccessAll,
 } from "@inrupt/solid-client"
 import { fetch as solid_fetch, getDefaultSession } from "@inrupt/solid-client-authn-browser"
 import { h } from "preact"
+import { useState } from "preact/hooks"
 import { get_new_knowledge_view_object } from "../knowledge_view/create_new_knowledge_view"
 
 import { is_defined } from "../shared/utils/is_defined"
-import { get_new_wcomponent_object } from "../shared/wcomponent/get_new_wcomponent_object"
+import { get_contextless_new_wcomponent_object, get_new_wcomponent_object } from "../shared/wcomponent/get_new_wcomponent_object"
+import type { KnowledgeView } from "../shared/wcomponent/interfaces/knowledge_view"
+import type { WComponent } from "../shared/wcomponent/interfaces/SpecialisedObjects"
 import type { RootState } from "../state/State"
 import type { SyncError } from "../state/sync/utils/errors"
 import { V1 } from "../state/sync/utils/solid"
 import { load_solid_data } from "../state/sync/utils/solid_load_data"
 import { save_solid_data } from "../state/sync/utils/solid_save_data"
-import { start_login } from "../sync/user_info/solid/handle_login"
+import { finish_login, start_login } from "../sync/user_info/solid/handle_login"
 
 import "./SandBox.css"
 
 
 
+const url_groups: { broker: string, pod_directory: string }[] = [
+    { broker: "https://broker.pod.inrupt.com", pod_directory: "https://pod.inrupt.com/ajp/data_curator_sandbox/" },
+    { broker: "https://trinpod.us/", pod_directory: "https://ajp.trinpod.us/" },
+]
+const urls = url_groups[0]!
+
+
 export function SandBoxSolid ()
 {
+    const [, update_view] = useState({})
+
     const solid_session = getDefaultSession()
 
-    return <div>
-        {solid_session.info.isLoggedIn ? "Logged in" : <div onClick={() => {start_login(solid_session, "https://broker.pod.inrupt.com")}}>Signin</div>}
+    const { isLoggedIn } = solid_session.info
+    finish_login().then(() =>
+    {
+        if (isLoggedIn !== solid_session.info.isLoggedIn) update_view({})
+    })
 
-        {solid_session.info.isLoggedIn && <div onClick={() => main()}>Save and load</div>}
-    </div>
+
+    if (!isLoggedIn) return (
+        <div onClick={() => start_login(solid_session, urls.broker)}>
+            Signin
+        </div>
+    )
+
+
+    return (
+        <div>
+            Logged in
+
+            <div onClick={() => save_and_load()}>Save and load</div>
+            <div onClick={() => get_acl()}>Get ACL</div>
+        </div>
+    )
 }
 
 
 
-async function main()
+async function save_and_load ()
 {
-    const url = "https://pod.inrupt.com/ajp/"
+    const url = urls.pod_directory
     const state: RootState = {
         user_info: {
-            solid_oidc_provider: "https://broker.pod.inrupt.com",
+            solid_oidc_provider: urls.broker,
             user_name: "abc",
             default_solid_pod_URL: url,
             custom_solid_pod_URLs: [],
             chosen_custom_solid_pod_URL_index: 0,
         }
-    } as any
+    } as Partial<RootState> as any
 
     await save_solid_data(state.user_info, {
-        knowledge_views: knowledge_views.map(j => JSON.parse(j)),
-        wcomponents: wcomponents.map(j => JSON.parse(j)),
+        knowledge_views,
+        wcomponents,
         perceptions: [],
     })
     const items = await load_solid_data(state)
@@ -62,5 +95,33 @@ async function main()
 }
 
 
-const knowledge_views: any[] = []
-const wcomponents: any[] = []
+const wc1 = get_contextless_new_wcomponent_object({ title: "wc1" })
+const wcomponents: WComponent[] = [wc1]
+
+const kv1 = get_new_knowledge_view_object({
+    title: "kv1",
+    wc_id_map: {
+        [wc1.id]: { left: 0, top: 0 },
+    },
+})
+const knowledge_views: KnowledgeView[] = [kv1]
+
+
+
+async function get_acl ()
+{
+    const url = "https://pod.inrupt.com/ajp/data_curator_v1/"
+
+    const dataset = await getSolidDatasetWithAcl(url, { fetch: solid_fetch })
+    // console .log("got dataset", dataset)
+
+    const accessByAgent = getAgentAccessAll(dataset)
+    if (!accessByAgent) return console.log("No accessByAgent for ", url)
+
+    // => accessByAgent is an object with Agent WebIDs as keys,
+    //    and their associated access object {read: <boolean>, ... } as values.
+    for (const [agent, access] of Object.entries(accessByAgent)) {
+        console.log(agent, access)
+    }
+
+}
