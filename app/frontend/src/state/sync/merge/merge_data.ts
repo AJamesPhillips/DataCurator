c76915821ca8a8701859ba1b62b641e572443f0d
Alexander James Phillips <ajp.global@gmail.com>
2021-09-27 11:25:21 +0100

    Add failing test for datetime fields in merge_data

--- app/frontend/src/state/sync/merge/merge_data.ts
+++ app/frontend/src/state/sync/merge/merge_data.ts
@@ -6,7 +6,8 @@ export interface MergeDataCoreArgs<U>
 {
     last_source_of_truth: U
     current_value: U
-    attempted_update_value: U
+    // source_of_truth represents the value returned from the server (either 200 or 409),
+    // NOT the value that was on the server before the attempted update.
     source_of_truth: U
     // i.e. 200 or 409 i.e. are you the last client to update this row or did a different
     // client already update it and you had an old modified_at datetimestamp?
@@ -16,13 +17,14 @@ export interface MergeDataCoreArgs<U>
 interface MergeDataArgs<U> extends MergeDataCoreArgs<U>
 {
     get_field_value: <T extends keyof U>(field: T, item: U) => any
-    get_custom_field_merger: <T extends keyof U>(field: T) => (undefined | ((args: MergeDataCoreArgs<U>) => { conflict: boolean, value: U[T] }))
+    get_custom_field_merger: <T extends keyof U>(field: T) => (undefined | ((args: MergeDataCoreArgs<U>) => { needs_save: boolean, unresolvable_conflict: boolean, value: U[T] }))
 }
 
 export interface MergeDataReturn<U>
 {
-    conflict: boolean
     value: U
+    needs_save: boolean
+    unresolvable_conflicted_fields: (keyof U)[]
 }
 
 export function merge_data <U extends Base> (args: MergeDataArgs<U>): MergeDataReturn<U>
@@ -30,27 +32,28 @@ export function merge_data <U extends Base> (args: MergeDataArgs<U>): MergeDataR
     const {
         last_source_of_truth,
         current_value,
-        attempted_update_value,
         source_of_truth,
-        update_successful,
         get_field_value,
         get_custom_field_merger,
     } = args
 
-    const fields = get_fields(attempted_update_value, current_value, last_source_of_truth, source_of_truth)
 
-    let conflict = false
-    let value = { ...last_source_of_truth }
+    let value = { ...source_of_truth }
+    let needs_save = false
+    let unresolvable_conflicted_fields: (keyof U)[] = []
 
+
+    const fields = get_fields(current_value, last_source_of_truth, source_of_truth)
     fields.forEach(field =>
     {
         const field_merger = get_custom_field_merger(field) || get_default_field_merger(field)
         const merge = field_merger(args)
-        // conflict = conflict || merge.conflict
+        needs_save = needs_save || merge.needs_save
+        if (merge.unresolvable_conflict) unresolvable_conflicted_fields.push(field)
         value[field] = merge.value
     })
 
-    return { conflict, value }
+    return { value, needs_save, unresolvable_conflicted_fields }
 }
 
 
@@ -74,12 +77,63 @@ function get_fields <U> (...items: U[]): (keyof U)[]
 
 function get_default_field_merger <U extends Base, T extends keyof U> (field: T)
 {
-    function default_field_merger (args: MergeDataCoreArgs<U>): { conflict: boolean, value: U[T] }
-    {
-        if (field === "modified_at") return { conflict: false, value: args.source_of_truth[field] }
+    const always_current_value_fields: Set<keyof U> = new Set([
+        "needs_save",
+        "saving",
+    ])
 
-        return { conflict: false, value: args.attempted_update_value[field] }
+    const always_source_of_truth_fields: Set<keyof U> = new Set([
+        "id",
+        "created_at",
+        "modified_at",
+    ])
+
+
+    function default_field_merger (args: MergeDataCoreArgs<U>): { needs_save: boolean, unresolvable_conflict: boolean, value: U[T] }
+    {
+        const source_of_truth = args.source_of_truth[field]
+        const current_value = args.current_value[field]
+
+
+        let needs_save = false
+        let unresolvable_conflict = false
+
+
+        if (always_current_value_fields.has(field))
+        {
+            return { needs_save, unresolvable_conflict, value: current_value }
+        }
+
+
+        if (always_source_of_truth_fields.has(field))
+        {
+            return { needs_save, unresolvable_conflict, value: source_of_truth }
+        }
+
+
+        const last_source_of_truth = args.last_source_of_truth[field]
+
+
+        let value: U[T]
+
+        if (args.update_successful || source_of_truth === last_source_of_truth)
+        {
+            value = current_value
+            needs_save = current_value !== source_of_truth
+        }
+        else
+        {
+            value = source_of_truth
+            if (current_value !== last_source_of_truth)
+            {
+                console.log("unresolvable_conflict with field: ", field, "last_source_of_truth", last_source_of_truth, "source_of_truth", source_of_truth, "current_value", current_value)
+                unresolvable_conflict = true
+            }
+        }
+
+        return { needs_save, unresolvable_conflict, value }
     }
+
 
     return default_field_merger
 }
--- app/frontend/src/state/sync/merge/merge_wcomponents.ts
+++ app/frontend/src/state/sync/merge/merge_wcomponents.ts
@@ -29,17 +29,23 @@ function get_custom_field_merger (field: keyof WComponent)
 function run_tests()
 {
     const dt1 = new Date("2021-01-01")
-    const dt2 = new Date("2021-02-02")
-    const dt3 = new Date("2021-03-03")
+    // either the modified_at values match (between attempted_update_value and the DB's initial value)
+    // and will result in an update, a new modified_at and a 200 response code.
+    // OR they will not match, the current DB value will be returned with a 409 response code.
+    const latest_modified_at = new Date("2021-02-02")
 
 
-    function dt_ms (date?: Date)
+    function dt_ms (date?: Date, warn_when_undefined = true)
     {
+        if (!date && warn_when_undefined)
+        {
+            throw new Error("Datetime is undefined.  Set warn_when_undefined = false?")
+        }
+
         return date ? date.getTime() : undefined
     }
 
 
-    test_should_handle_create_on_client()
     test_should_handle_update_on_client()
     test_should_handle_nonconflicting_updates()
     test_should_handle_conflicting_updates()
@@ -48,73 +54,242 @@ function run_tests()
     test_should_handle_nonconflicting_updates_with_multiple_client_updates()
     test_should_handle_conflicting_updates_with_multiple_client_updates()
 
+    test_should_handle_non_and_conflicting_updates()
+    test_should_handle_non_and_conflicting_updates_with_multiple_client_updates()
+
     test_should_handle_different_custom_created_at()
 
-
-
-    function test_should_handle_create_on_client ()
-    {
-
-    }
 
 
     function test_should_handle_update_on_client ()
     {
         const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
-            title: "TA", description: "DA", modified_at: dt1,
+            title: "TA", modified_at: dt1,
         })
         const current_value: WComponent = { ...last_source_of_truth, title: "TB", needs_save: true, saving: true }
-        const attempted_update_value: WComponent = { ...current_value, needs_save: undefined, saving: undefined }
-        const source_of_truth: WComponent = { ...current_value, needs_save: undefined, saving: undefined, modified_at: dt2 }
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TB", modified_at: latest_modified_at }
 
         const merge = merge_wcomponent({
             last_source_of_truth,
             current_value,
-            attempted_update_value,
             source_of_truth,
-            update_successful: true,
+            update_successful: true, // 200
         })
 
-        test(merge.conflict, false)
-        test(dt_ms(merge.value.modified_at), dt_ms(source_of_truth.modified_at))
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
         test(merge.value.title, "TB")
-        test(merge.value.description, "DA")
+
+        test(merge.needs_save, false)
+        test(merge.unresolvable_conflicted_fields, [])
     }
 
 
     function test_should_handle_nonconflicting_updates ()
     {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", description: "DA", modified_at: dt1,
+        })
+        const current_value: WComponent = { ...last_source_of_truth, title: "TB", needs_save: true, saving: true }
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TA", description: "DX", modified_at: latest_modified_at }
 
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: false, // 409
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TB")
+        test(merge.value.description, "DX")
+
+        test(merge.needs_save, true)
+        test(merge.unresolvable_conflicted_fields, [])
     }
 
 
     function test_should_handle_conflicting_updates ()
     {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", modified_at: dt1,
+        })
+        const current_value: WComponent = { ...last_source_of_truth, title: "TB", needs_save: true, saving: true }
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TX", modified_at: latest_modified_at }
 
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: false, // 409
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TX")
+
+        // There's nothing here that needs to be updated
+        test(merge.needs_save, false)
+        test(merge.unresolvable_conflicted_fields, ["title"])
     }
 
 
     function test_should_handle_multiple_updates_on_client ()
     {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", modified_at: dt1,
+        })
+        // Client makes second change from TB to TC
+        const current_value: WComponent = { ...last_source_of_truth, title: "TC", needs_save: true, saving: true }
+        // Represents first change from TA to TB
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TB", modified_at: latest_modified_at }
 
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: true, // 200
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TC")
+        test(merge.needs_save, true)
+        test(merge.unresolvable_conflicted_fields, [])
     }
 
 
     function test_should_handle_nonconflicting_updates_with_multiple_client_updates ()
     {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", description: "DA", modified_at: dt1,
+        })
+        // Client makes second change from TB to TC
+        const current_value: WComponent = { ...last_source_of_truth, title: "TC", needs_save: true, saving: true }
+        // Represents first change from TA to TB
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TA", description: "DX", modified_at: latest_modified_at }
 
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: false, // 409
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TC")
+        test(merge.value.description, "DX")
+
+        test(merge.needs_save, true)
+        test(merge.unresolvable_conflicted_fields, [])
     }
 
 
     function test_should_handle_conflicting_updates_with_multiple_client_updates ()
     {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", modified_at: dt1,
+        })
+        // Client makes second change from TB to TC
+        const current_value: WComponent = { ...last_source_of_truth, title: "TC", needs_save: true, saving: true }
+        // Represents first change from TA to TB
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TX", modified_at: latest_modified_at }
 
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: false, // 409
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TX")
+
+        // There's nothing here that needs to be updated
+        test(merge.needs_save, false)
+        test(merge.unresolvable_conflicted_fields, ["title"])
+    }
+
+
+    function test_should_handle_non_and_conflicting_updates ()
+    {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", description: "DA", modified_at: dt1,
+        })
+        const current_value: WComponent = { ...last_source_of_truth, title: "TB", description: "DB", needs_save: true, saving: true }
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB", description: "DB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TA", description: "DX", modified_at: latest_modified_at }
+
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: false, // 409
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TB")
+        test(merge.value.description, "DX")
+
+        // Need to save the title
+        test(merge.needs_save, true)
+        test(merge.unresolvable_conflicted_fields, ["description"])
+    }
+
+
+    function test_should_handle_non_and_conflicting_updates_with_multiple_client_updates ()
+    {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            title: "TA", description: "DA", modified_at: dt1,
+        })
+        // Client makes second change from TB to TC and DB to DC
+        const current_value: WComponent = { ...last_source_of_truth, title: "TC", description: "DC", needs_save: true, saving: true }
+        // Represents first change from TA to TB and DA to DB
+        const attempted_update_value: WComponent = { ...last_source_of_truth, title: "TB", description: "DB" }
+        const source_of_truth: WComponent = { ...last_source_of_truth, title: "TA", description: "DX", modified_at: latest_modified_at }
+
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: false, // 409
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(merge.value.title, "TC")
+        test(merge.value.description, "DX")
+
+        // Need to save the title
+        test(merge.needs_save, true)
+        test(merge.unresolvable_conflicted_fields, ["description"])
     }
 
 
     function test_should_handle_different_custom_created_at ()
     {
+        const last_source_of_truth: WComponent = get_contextless_new_wcomponent_object({
+            custom_created_at: new Date("2021"), modified_at: dt1,
+        })
+        const current_value: WComponent = { ...last_source_of_truth, custom_created_at: new Date("2015"), needs_save: true, saving: true }
+        const attempted_update_value: WComponent = { ...last_source_of_truth, custom_created_at: new Date("2015") }
+        const source_of_truth: WComponent = { ...last_source_of_truth, custom_created_at: new Date("2015"), modified_at: latest_modified_at }
 
+        const merge = merge_wcomponent({
+            last_source_of_truth,
+            current_value,
+            source_of_truth,
+            update_successful: true, // 200
+        })
+
+        test(dt_ms(merge.value.modified_at), dt_ms(latest_modified_at))
+        test(new Date("2021"), new Date("2021"))
+        test(merge.value.custom_created_at, new Date("2015"))
+
+        test(merge.needs_save, false)
+        test(merge.unresolvable_conflicted_fields, [])
     }
 }
 
