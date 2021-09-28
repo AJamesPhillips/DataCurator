4e516c68475b4b10c2eff1d634a169c5504ebf8c
Alexander James Phillips <ajp.global@gmail.com>
2021-09-28 19:24:43 +0100

    Add unique auto incrementing short_id to wcomponents and knowledge views

--- app/server/supabase_setup/4_kv_and_components/0100_tables.sql
+++ app/server/supabase_setup/4_kv_and_components/0100_tables.sql
@@ -1,15 +1,18 @@
 
-DROP TABLE knowledge_views
+
 
 CREATE TABLE IF NOT EXISTS knowledge_views (
   id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  short_id bigserial,
   modified_at timestamp without time zone DEFAULT now()::timestamp(3) NOT NULL,
   base_id bigint NOT NULL,
   title text NOT NULL,
   json json NOT NULL,
   CONSTRAINT fk_knowledge_views_base
     FOREIGN KEY(base_id)
-	  REFERENCES bases(id)
+    REFERENCES bases(id),
+  CONSTRAINT knowledge_views_unique_short_ids
+    UNIQUE (short_id)
 );
 
 
@@ -17,11 +20,14 @@ CREATE TABLE IF NOT EXISTS knowledge_views (
 
 CREATE TABLE IF NOT EXISTS wcomponents (
   id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+  short_id bigserial,
   modified_at timestamp without time zone DEFAULT now()::timestamp(3) NOT NULL,
   base_id bigint NOT NULL,
   title text NOT NULL,
   json json NOT NULL,
   CONSTRAINT fk_wcomponents_base
     FOREIGN KEY(base_id)
-	  REFERENCES bases(id)
+    REFERENCES bases(id),
+  CONSTRAINT wcomponents_unique_short_ids
+    UNIQUE (short_id)
 );
