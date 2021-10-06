54fa92c78393637fa245f747213c898a7a87439a
Alexander James Phillips <ajp.global@gmail.com>
2021-10-06 09:54:10 +0100

    Max created_at datetime to not hide new VAP sets

--- app/frontend/src/knowledge/multiple_values/ValueAndPredictionSets.tsx
+++ app/frontend/src/knowledge/multiple_values/ValueAndPredictionSets.tsx
@@ -1,6 +1,7 @@
 import { FunctionalComponent, h } from "preact"
 import { connect, ConnectedProps } from "react-redux"
 
+import { get_created_at_ms } from "../../shared/utils_datetime/utils_datetime"
 import type { VAPsType } from "../../shared/wcomponent/interfaces/generic_value"
 import type { ValuePossibilitiesById } from "../../shared/wcomponent/interfaces/possibility"
 import type {
@@ -10,10 +11,10 @@ import type {
 import {
     partition_and_prune_items_by_datetimes_and_versions,
 } from "../../shared/wcomponent/value_and_prediction/utils"
+import { ACTIONS } from "../../state/actions"
 import type { RootState } from "../../state/State"
 import { selector_chosen_base_id } from "../../state/user_info/selector"
 import { ValueAndPredictionSetsComponent } from "./ValueAndPredictionSetsComponent"
-import { get_max_value_possibilities_order } from "./value_possibilities/get_max_value_possibilities_order"
 import { update_value_possibilities } from "./value_possibilities/update_possibilities_with_VAPs"
 
 
@@ -36,23 +37,29 @@ const map_state = (state: RootState) =>
         creation_context: state.creation_context,
         editing: !state.display_options.consumption_formatting,
         base_id: selector_chosen_base_id(state),
+        current_created_at_ms: state.routing.args.created_at_ms,
     }
 }
 
 
+const map_dispatch = {
+    change_route: ACTIONS.routing.change_route,
+}
 
-const connector = connect(map_state)
+
+const connector = connect(map_state, map_dispatch)
 type Props = ConnectedProps<typeof connector> & OwnProps
 
 
 function _ValueAndPredictionSets (props: Props)
 {
     const {
-        wcomponent_id, values_and_prediction_sets, VAPs_represent, base_id = -1,
+        wcomponent_id, values_and_prediction_sets: orig_values_and_prediction_sets, VAPs_represent, base_id = -1,
+        current_created_at_ms, change_route,
     } = props
 
     const { invalid_future_items, past_items, present_items, future_items, previous_versions_by_id } = partition_and_prune_items_by_datetimes_and_versions({
-        items: values_and_prediction_sets,
+        items: orig_values_and_prediction_sets,
         created_at_ms: props.created_at_ms,
         sim_ms: props.sim_ms,
     })
@@ -62,15 +69,24 @@ function _ValueAndPredictionSets (props: Props)
 
         item_descriptor="Value Prediction"
         VAPs_represent={VAPs_represent}
-        update_values_and_predictions={values_and_prediction_sets =>
+        update_values_and_predictions={new_values_and_prediction_sets =>
         {
-            const value_possibilities = update_value_possibilities(props.value_possibilities, values_and_prediction_sets)
-            props.update_values_and_predictions({ value_possibilities, values_and_prediction_sets })
+            const value_possibilities = update_value_possibilities(props.value_possibilities, new_values_and_prediction_sets)
+            props.update_values_and_predictions({
+                value_possibilities, values_and_prediction_sets: new_values_and_prediction_sets
+            })
+
+            const orig_latest_created_at_ms = get_latest_created_at_VAP_set_ms(orig_values_and_prediction_sets, current_created_at_ms)
+            const new_latest_created_at_ms = get_latest_created_at_VAP_set_ms(new_values_and_prediction_sets, current_created_at_ms)
+            if (new_latest_created_at_ms > orig_latest_created_at_ms)
+            {
+                change_route({ args: { created_at_ms: new_latest_created_at_ms + (1000 * 60) } })
+            }
         }}
 
         value_possibilities={props.value_possibilities}
 
-        values_and_prediction_sets={values_and_prediction_sets}
+        values_and_prediction_sets={orig_values_and_prediction_sets}
         invalid_future_items={invalid_future_items}
         past_items={past_items}
         present_items={present_items}
@@ -84,3 +100,16 @@ function _ValueAndPredictionSets (props: Props)
 }
 
 export const ValueAndPredictionSets = connector(_ValueAndPredictionSets) as FunctionalComponent<OwnProps>
+
+
+
+function get_latest_created_at_VAP_set_ms (values_and_prediction_sets: StateValueAndPredictionsSet[], latest_ms = 0)
+{
+
+    values_and_prediction_sets.forEach(VAP_set =>
+    {
+        latest_ms = Math.max(get_created_at_ms(VAP_set), latest_ms)
+    })
+
+    return latest_ms
+}
