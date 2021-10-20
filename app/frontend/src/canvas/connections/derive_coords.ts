d85ffb2985458f6a46e2a7dd9942b1dddb357cc3
Alexander James Phillips <ajp.global@gmail.com>
2021-10-20 16:49:24 +0100

    Better curved connecting lines

--- app/frontend/src/canvas/connections/derive_coords.ts
+++ app/frontend/src/canvas/connections/derive_coords.ts
@@ -38,18 +38,36 @@ export function derive_coords (args: DeriveCoordsArgs )
 
     if (line_behaviour === undefined || line_behaviour === "curve")
     {
-        const magnitude = get_magnitude(x1, y1, x2, y2) / 3
-
-        const start_angle = get_angle_from_start_connector(angle, from_connection_type.direction)
-        end_angle = get_angle_from_end_connector(angle, to_connection_type.direction)
-
-        const control_point_magnitude = bounded(magnitude, 10, 300)
-        control_point1 = to_vec(start_angle, control_point_magnitude)
-        control_point2 = to_vec(end_angle, control_point_magnitude)
+        if (x2 < x1 + 20)
+        {
+            ({ end_angle, control_point1, control_point2 } = loop_curve(x1, y1, x2, y2, angle, from_connection_type, end_angle, to_connection_type, control_point1, control_point2))
+        }
+        else
+        {
+            end_angle = rads._180
+            const xc = (x2 - x1) / 2
+            control_point1 = { x: xc, y: 0 }
+            control_point2 = { x: -xc, y: 0 }
+        }
     }
 
 
     return {
         x1, y1, x2, y2, control_point1, control_point2, end_angle
     }
+}
+
+
+
+function loop_curve (x1: number, y1: number, x2: number, y2: number, angle: number, from_connection_type: ConnectionTerminalType, end_angle: number, to_connection_type: ConnectionTerminalType, control_point1: Vector, control_point2: Vector)
+{
+    const magnitude = get_magnitude(x1, y1, x2, y2) / 3
+
+    const start_angle = get_angle_from_start_connector(angle, from_connection_type.direction)
+    end_angle = get_angle_from_end_connector(angle, to_connection_type.direction)
+
+    const control_point_magnitude = bounded(magnitude, 10, 300)
+    control_point1 = to_vec(start_angle, control_point_magnitude)
+    control_point2 = to_vec(end_angle, control_point_magnitude)
+    return { end_angle, control_point1, control_point2 }
 }
