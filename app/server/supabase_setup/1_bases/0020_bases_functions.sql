253dfe391770b5d6eaf848bcad565b2a153cacd3
Alexander James Phillips <ajp.global@gmail.com>
2021-10-26 19:27:58 +0100

    Allow users to see public base content

--- app/server/supabase_setup/1_bases/0020_bases_functions.sql
+++ app/server/supabase_setup/1_bases/0020_bases_functions.sql
@@ -27,3 +27,18 @@ as $$
   from bases
   where bases.public_read = true;
 $$;
+
+
+
+
+CREATE OR REPLACE FUNCTION ids_of_public_bases()
+returns setof int8
+stable
+language sql
+security definer
+SET search_path = public
+as $$
+  select id
+  from bases
+  where bases.public_read = true;
+$$;
--- app/server/supabase_setup/4_kv_and_components/0120_policy.sql
+++ app/server/supabase_setup/4_kv_and_components/0120_policy.sql
@@ -16,6 +16,7 @@ CREATE policy "Other users can insert knowledge_views into bases they are editor
 -- Disabled as they now do this through the version safe `update_knowledge_view`
 -- CREATE policy "Other users can update knowledge_views they are editors of" on public.knowledge_views for update using ( knowledge_views.base_id in ( select get_bases_editable_or_viewable_for_authorised_user(false))) with check ( knowledge_views.base_id in ( select get_bases_editable_or_viewable_for_authorised_user(false)));
 
+CREATE policy "Other users can read knowledge_views of public bases" on public.knowledge_views for select using ( knowledge_views.base_id in (select ids_of_public_bases()) );
 
 
 
@@ -26,3 +27,5 @@ CREATE policy "Users can insert their own bases wcomponents" on public.wcomponen
 
 CREATE policy "Other users can read wcomponents they are viewers or editors of" on public.wcomponents for select using ( wcomponents.base_id in ( select get_bases_editable_or_viewable_for_authorised_user(true)));
 CREATE policy "Other users can insert wcomponents into bases they are editors of" on public.wcomponents for insert with check ( wcomponents.base_id in ( select get_bases_editable_or_viewable_for_authorised_user(false)));
+
+CREATE policy "Other users can read wcomponents of public bases" on public.wcomponents for select using ( wcomponents.base_id in (select ids_of_public_bases()) );
