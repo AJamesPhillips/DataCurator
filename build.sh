7ca1596c5cc55b26cacb6f02d4a30e72d367ff26
Alexander James Phillips <ajp.global@gmail.com>
2021-10-28 18:16:54 +0100

    Allow datacurator.org to serve /sim

--- build.sh
+++ build.sh
@@ -11,7 +11,9 @@ cd -
 cd app/frontend
 npm run build
 mkdir ../../../data-curator-build/app
+mkdir ../../../data-curator-build/sim
 cp ../../../data-curator-build/index.html ../../../data-curator-build/app/index.html
+cp ../../../data-curator-build/index.html ../../../data-curator-build/sim/index.html
 
 echo "datacurator.org" > ../../../data-curator-build/CNAME
 touch ../../../data-curator-build/.nojekyll
