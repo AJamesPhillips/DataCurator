0b6353a2c77acf08648df98d2a307cddda0f3e20
Alexander James Phillips <ajp.global@gmail.com>
2021-11-10 20:43:00 +0000

    Allow side panel to open in more cases where last action is route change

--- app/frontend/src/state/controls/subscribers.ts
+++ app/frontend/src/state/controls/subscribers.ts
@@ -1,4 +1,5 @@
 import { ACTIONS } from "../actions"
+import { is_change_route } from "../routing/actions"
 import type { StoreType } from "../store"
 
 
@@ -24,9 +25,21 @@ function show_side_panel_on_change_route (store: StoreType)
         const state = store.getState()
         const current_route = state.routing.route
         const { display_side_panel } = state.controls
-
-        if (last_route === current_route) return
         if (display_side_panel) return
+        if (last_route === current_route)
+        {
+            if (!state.last_action) return
+            if (!is_change_route(state.last_action)) return
+            if (!state.last_action.route) return
+            // Probably the user has repeated an action to request a route but the side panel is not open
+            // This is not 100% guaranteed so we might need to remove this functionality but at the moment
+            // I am imagining that if:
+            //  * the side panel is closed
+            //  * the filter menu is already selected
+            //  * a filter is applied
+            // If the user then clicks the warning that a filter is applied then these checks will allow
+            // for the sidepanel to be opened.
+        }
 
         store.dispatch(ACTIONS.controls.toggle_display_side_panel())
     })
