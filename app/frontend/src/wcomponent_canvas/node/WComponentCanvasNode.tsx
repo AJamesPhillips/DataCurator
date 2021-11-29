365382024e374c9a549f5376be30c47d1af5ab67
Alexander James Phillips <ajp.global@gmail.com>
2021-11-29 21:15:39 +0000

    Do not use compact node title when current item

--- app/frontend/src/wcomponent_canvas/node/WComponentCanvasNode.tsx
+++ app/frontend/src/wcomponent_canvas/node/WComponentCanvasNode.tsx
@@ -212,7 +212,7 @@ function _WComponentCanvasNode (props: Props)
     const title = !wcomponent ? "&lt;Not found&gt;" : get_title({ wcomponent, rich_text: true, wcomponents_by_id, wc_id_to_counterfactuals_map, created_at_ms, sim_ms })
 
 
-    const show_all_details = is_editing || is_current_item
+    const show_all_details = is_editing //|| is_current_item
 
     const use_styles = makeStyles(theme => ({
         sizer: {
