e35d368f912481a9fb7382a26ffbe80c33b1d001
Alexander James Phillips <ajp.global@gmail.com>
2021-12-19 20:26:51 +0000

    Correctly sort prioritisations for Priorities view

--- app/frontend/src/priorities/PrioritiesView.tsx
+++ app/frontend/src/priorities/PrioritiesView.tsx
@@ -1,18 +1,17 @@
 import { FunctionalComponent, h } from "preact"
+import { useMemo } from "preact/hooks"
 import { connect, ConnectedProps } from "react-redux"
 
 import { Canvas } from "../canvas/Canvas"
 import { round_coordinate_small_step } from "../canvas/position_utils"
-import { calculate_canvas_x_for_datetime, calculate_canvas_x_for_wcomponent_temporal_uncertainty, default_time_origin_parameters } from "../knowledge_view/datetime_line"
+import { calculate_canvas_x_for_datetime, default_time_origin_parameters } from "../knowledge_view/datetime_line"
 import { KnowledgeGraphTimeMarkers } from "../knowledge_view/KnowledgeGraphTimeMarkers"
 import { MainArea } from "../layout/MainArea"
 import { get_uncertain_datetime } from "../shared/uncertainty/datetime"
 import { sort_list } from "../shared/utils/sort"
-import { get_created_at_ms } from "../shared/utils_datetime/utils_datetime"
 import { get_current_composed_knowledge_view_from_state } from "../state/specialised_objects/accessors"
 import type { RootState } from "../state/State"
 import type { WComponentPrioritisation } from "../wcomponent/interfaces/priorities"
-import { DailyActionNode } from "./DailyActionNode"
 import { ProjectPriorityNode } from "./old_project_priorities/ProjectPriorityNode"
 
 
@@ -43,78 +42,106 @@ const get_svg_children = (props: Props) =>
 }
 
 
+interface HasCalcdUncertainDatetime
+{
+    calcd_uncertain_datetime: Date
+}
+function has_calcd_datetime <U> (p: U & Partial<HasCalcdUncertainDatetime>): p is U & HasCalcdUncertainDatetime
+{
+    return !!p.calcd_uncertain_datetime
+}
+
+interface WComponentPrioritisationWithCalcdDatetime extends WComponentPrioritisation, HasCalcdUncertainDatetime {}
+
 
 const get_children = (props: Props) =>
 {
-    let { prioritisations = [] } = props
-    prioritisations = sort_list(prioritisations, p => get_created_at_ms(p), "ascending")
-        .filter(p => !!get_uncertain_datetime(p.datetime))
-
-    let offset = 0
-    const goal_or_action_id_to_offset: {[goal_or_action_id: string]: number} = {}
-    const prioritised_goal_or_action_data: {
-        prioritisation_id: string, goal_or_action_id: string, effort: number
-    }[] = []
-    const prioritisation_data_by_id: {[prioritisation_id: string]: {
-        total_effort: number, start_date: Date, end_date: Date
-    }} = {}
-
-    prioritisations.forEach((prioritisation, prioritisation_index) =>
+    const { prioritisations = [] } = props
+    const { prioritised_goal_or_action_data, goal_or_action_id_to_offset, prioritisation_data_by_id } = useMemo(() =>
     {
-        const start_date = get_uncertain_datetime(prioritisation.datetime)
-        if (!start_date) return // type guard, previous filter should ensure it has datetime with a value
+        let prioritisations_with_datetime: WComponentPrioritisationWithCalcdDatetime[] = []
 
-        let total_effort = 0
-        Object.entries(prioritisation.goals).forEach(([goal_or_action_id, prioritisation_entry]) =>
+        prioritisations_with_datetime = prioritisations
+            .map(prioritisation => ({
+                ...prioritisation,
+                calcd_uncertain_datetime: get_uncertain_datetime(prioritisation.datetime),
+            }))
+            .filter(has_calcd_datetime)
+
+        prioritisations_with_datetime = sort_list(prioritisations_with_datetime, p => p.calcd_uncertain_datetime.getTime(), "ascending")
+            .filter(p => !!get_uncertain_datetime(p.datetime))
+
+
+        let offset = 0
+        const goal_or_action_id_to_offset: {[goal_or_action_id: string]: number} = {}
+        const prioritised_goal_or_action_data: {
+            prioritisation_id: string, goal_or_action_id: string, effort: number
+        }[] = []
+        const prioritisation_data_by_id: {[prioritisation_id: string]: {
+            total_effort: number, start_date: Date, end_date: Date
+        }} = {}
+
+        prioritisations_with_datetime.forEach((prioritisation, prioritisation_index) =>
         {
-            total_effort += prioritisation_entry.effort
+            let total_effort = 0
+            Object.entries(prioritisation.goals).forEach(([goal_or_action_id, prioritisation_entry]) =>
+            {
+                total_effort += prioritisation_entry.effort
 
-            prioritised_goal_or_action_data.push({
-                prioritisation_id: prioritisation.id,
-                goal_or_action_id,
-                effort: prioritisation_entry.effort,
+                prioritised_goal_or_action_data.push({
+                    prioritisation_id: prioritisation.id,
+                    goal_or_action_id,
+                    effort: prioritisation_entry.effort,
+                })
+
+                if (goal_or_action_id_to_offset[goal_or_action_id] !== undefined) return
+                goal_or_action_id_to_offset[goal_or_action_id] = offset++
             })
 
-            if (goal_or_action_id_to_offset[goal_or_action_id] !== undefined) return
-            goal_or_action_id_to_offset[goal_or_action_id] = offset++
+
+            const next_prioritisation = prioritisations_with_datetime[prioritisation_index + 1]
+            const end_date = get_uncertain_datetime(next_prioritisation?.datetime) || new Date()
+
+            prioritisation_data_by_id[prioritisation.id] = {
+                total_effort,
+                start_date: prioritisation.calcd_uncertain_datetime,
+                end_date,
+            }
         })
 
+        return { prioritised_goal_or_action_data, goal_or_action_id_to_offset, prioritisation_data_by_id }
+    }, [prioritisations])
 
-        const next_prioritisation = prioritisations[prioritisation_index + 1]
-        const end_date = get_uncertain_datetime(next_prioritisation?.datetime) || new Date()
-
-        prioritisation_data_by_id[prioritisation.id] = {
-            total_effort,
-            start_date,
-            end_date,
-        }
-    })
 
 
     const { time_origin_ms, time_origin_x, time_scale } = default_time_origin_parameters(props)
 
-    const elements: h.JSX.Element[] = prioritised_goal_or_action_data.map(({ prioritisation_id, goal_or_action_id, effort }) =>
-    {
-        const offset = goal_or_action_id_to_offset[goal_or_action_id] || 0
-        const { total_effort, start_date, end_date } = prioritisation_data_by_id[prioritisation_id]!
+    const elements: h.JSX.Element[] = useMemo(() => prioritised_goal_or_action_data.map(({ prioritisation_id, goal_or_action_id, effort }) =>
+        {
+            const offset = goal_or_action_id_to_offset[goal_or_action_id] || 0
+            const { total_effort, start_date, end_date } = prioritisation_data_by_id[prioritisation_id]!
 
-        const x = round_coordinate_small_step(calculate_canvas_x_for_datetime({
-            datetime: start_date, time_origin_ms, time_origin_x, time_scale,
-        }))
-        const x2 = round_coordinate_small_step(calculate_canvas_x_for_datetime({
-            datetime: end_date, time_origin_ms, time_origin_x, time_scale,
-        }))
+            const x = round_coordinate_small_step(calculate_canvas_x_for_datetime({
+                datetime: start_date, time_origin_ms, time_origin_x, time_scale,
+            }))
+            const x2 = round_coordinate_small_step(calculate_canvas_x_for_datetime({
+                datetime: end_date, time_origin_ms, time_origin_x, time_scale,
+            }))
 
-        return <ProjectPriorityNode
-            effort={effort / total_effort}
-            wcomponent_id={goal_or_action_id}
-            x={x}
-            y={100 * offset}
-            width={x2 - x}
-            height={100}
-            display={true}
-        />
-    })
+            return <ProjectPriorityNode
+                effort={effort / total_effort}
+                wcomponent_id={goal_or_action_id}
+                x={x}
+                y={100 * offset}
+                width={x2 - x}
+                height={100}
+                display={true}
+            />
+        })
+    , [
+        prioritised_goal_or_action_data, goal_or_action_id_to_offset, prioritisation_data_by_id,
+        time_origin_ms, time_origin_x, time_scale,
+    ])
 
     // elements.push(<DailyActionNode
     //     action_ids={[]} width={10} height={10} display={true} x={x1} y={0}
