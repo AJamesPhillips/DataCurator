08eb109a7b70c399daaf45b6bc01a0ddd287c8ba
Alexander James Phillips <ajp.global@gmail.com>
2022-01-03 22:27:39 +0000

    Only clear multi or single wcomponent sidepanel view on canvas right click

--- app/frontend/src/state/specialised_objects/subscribers/cancel_selected_wcomponents_on_right_click.ts
+++ app/frontend/src/state/specialised_objects/subscribers/cancel_selected_wcomponents_on_right_click.ts
@@ -12,6 +12,11 @@ export function cancel_selected_wcomponents_on_right_click (store: Store<RootSta
     pub_sub.canvas.sub("canvas_right_click", (right_click: CanvasPointerEvent) =>
     {
         store.dispatch(ACTIONS.specialised_object.clear_selected_wcomponents({}))
-        store.dispatch(ACTIONS.routing.change_route({ route: "wcomponents", sub_route: null, item_id: null }))
+
+        const { route, item_id, sub_route } = store.getState().routing
+        if ((route === "wcomponents" && item_id) || sub_route === "wcomponents_edit_multiple" )
+        {
+            store.dispatch(ACTIONS.routing.change_route({ route: "wcomponents", sub_route: null, item_id: null }))
+        }
     })
 }
