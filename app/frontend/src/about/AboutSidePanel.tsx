02e14b68326bd2c6178da30ad19d16deb48fe0e6
Alexander James Phillips <ajp.global@gmail.com>
2022-01-27 15:20:13 +0000

    2022-01-27

--- app/frontend/src/about/AboutSidePanel.tsx
+++ app/frontend/src/about/AboutSidePanel.tsx
@@ -25,7 +25,7 @@ function _AboutSidePanel (props: Props)
     return <div>
         {!props.ready && <div>Loading...</div>}
 
-        <span className="description_label">Version</span> <b>2022-01-26</b>
+        <span className="description_label">Version</span> <b>2022-01-27</b>
     </div>
 }
 
