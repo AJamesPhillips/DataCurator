2a07e1576bd7246cab3d166f7bd6f7999d949fe3
Alexander James Phillips <ajp.global@gmail.com>
2022-04-06 21:57:57 +0100

    2022-04-06

--- app/frontend/src/about/AboutSidePanel.tsx
+++ app/frontend/src/about/AboutSidePanel.tsx
@@ -21,7 +21,7 @@ type Props = ConnectedProps<typeof connector> & OwnProps
 function _AboutSidePanel (props: Props)
 {
     return <div>
-        <span className="description_label">Version</span> <b>2022-04-05</b>
+        <span className="description_label">Version</span> <b>2022-04-06</b>
     </div>
 }
 
