2deb66f44b4e199283695fc8308a643fa77542bb
Alexander James Phillips <ajp.global@gmail.com>
2022-04-26 23:28:31 +0100

    Default frame is more transparent

--- app/frontend/src/wcomponent_form/wcomponent_knowledge_view_form/default_frame_color.ts
+++ app/frontend/src/wcomponent_form/wcomponent_knowledge_view_form/default_frame_color.ts
@@ -1,4 +1,4 @@
 import type { Color } from "../../shared/interfaces/color";
 
 
-export const default_frame_color: Color = { r: 175, g: 175, b: 175, a: 0.18 }
+export const default_frame_color: Color = { r: 175, g: 175, b: 175, a: 0.11 }
