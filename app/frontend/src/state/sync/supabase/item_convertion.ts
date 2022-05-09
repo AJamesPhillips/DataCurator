78269e539aed98022af94ff91b0fc939395989da
Alexander James Phillips <ajp.global@gmail.com>
2022-05-09 16:32:51 +0100

    Remove base_id from json blob sent to and received from server

--- app/frontend/src/state/sync/supabase/item_convertion.ts
+++ app/frontend/src/state/sync/supabase/item_convertion.ts
@@ -16,10 +16,16 @@ export function app_item_to_supabase <U extends Base & CommonFields> (item: U, b
     if (base_id === undefined) throw new Error("Must provide base_id for app_item_to_supabase")
 
     const json = clean_base_object_of_sync_meta_fields(item)
+    // base_id is first classed as a DB field, deleting it means we don't need to sync it between
+    // this field and json.base_id
+    delete (json as any).base_id
 
     const supabase_item: SupabaseWriteItem<U> = {
         id: item.id,
         modified_at: (item.modified_at ? item.modified_at.toISOString() : undefined) as any,
+        // Only used for creating the component, ignored for update due to custom function that
+        // has to check modified_at datetime and also base_id of actual item in DB rather than what
+        // is claimed here.
         base_id,
         json,
         // Not needed for update, only needed for create
@@ -37,6 +43,11 @@ export function supabase_item_to_app <U> (item: SupabaseReadItem<U>): U
     // Append `Z` for datetime value from server as it does not get stored with timezone and javascript's local
     // Date will parse it incorrectly.
     const modified_at_date = modified_at ? new Date(modified_at + "Z") : undefined
+
+    // base_id was previously being duplicated in the json field.  Delete it here as well before using
+    // the actual base_id value from the database field.
+    delete (json as any).base_id
+
     // Ensure all the fields that are edited in the db are set correctly in the json data.
     // Do not update title.  This should only be edited by the client app.  The canonical
     // value is in the DB's json field not the title field.
