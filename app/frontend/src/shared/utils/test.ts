fd7d1d0bfe9210d3d351ab44576a51efd0c80fe2
Alexander James Phillips <ajp.global@gmail.com>
2023-09-01 14:14:15 +0100

    Add skip function to test function

--- app/frontend/src/shared/utils/test.ts
+++ app/frontend/src/shared/utils/test.ts
@@ -1,8 +1,18 @@
 import { stable_stringify } from "./stable_stringify"
 
 
+interface TestFn
+{
+    <T>(got: T, expected: T, description?: string, sort_items?: boolean): void
+}
 
-export function test <T> (got: T, expected: T, description="", sort_items=true)
+interface Test extends TestFn
+{
+    skip: <T>(got: T, expected: T, description?: string, sort_items?: boolean) => void
+}
+
+
+const test_fn: TestFn = <T>(got: T, expected: T, description="", sort_items=true) =>
 {
     const str_got = sort_items ? stable_stringify(got) : JSON.stringify(got)
     const str_expected = sort_items ? stable_stringify(expected) : JSON.stringify(expected)
@@ -10,6 +20,12 @@ export function test <T> (got: T, expected: T, description="", sort_items=true)
     const pass = str_got === str_expected
     if (pass) console. log ("pass  " + description)
     else console.error (`fail: "${str_got}" !== "${str_expected}"  ${description}`)
+}
+
+export const test: Test = test_fn as any
+test.skip = <T>(got: T, expected: T, description="", sort_items=true) =>
+{
+    console. log ("skipping  " + description)
 }
 
 
