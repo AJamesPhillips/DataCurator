a3130367d5613723dba027c7b6a9a60adcb61211
Alexander James Phillips <ajp.global@gmail.com>
2023-09-05 13:15:48 +0100

    Remove babel plugin that silences material-ui error

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -3,13 +3,5 @@ import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact({
-    babel: {
-      plugins: [
-        // Adding this seems to silence a material-ui error about:
-        // `Following component has two or more children with the same key attribute: "1633093980000"`
-        ["@babel/plugin-transform-react-jsx-source"]
-      ]
-    }
-  })],
+  plugins: [preact()],
 })
