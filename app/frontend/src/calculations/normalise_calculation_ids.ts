2767cec1e7cf0399ba878ebdb2bacae3c921b7fd
Alexander James Phillips <ajp.global@gmail.com>
2023-09-25 16:59:36 +0100

    Refactor normalise_calculation_ids

--- app/frontend/src/calculations/normalise_calculation_ids.ts
+++ app/frontend/src/calculations/normalise_calculation_ids.ts
@@ -17,6 +17,30 @@ export function normalise_calculation_ids (equation: string, uuid_v4s: string[])
 {
     let converted_equation = equation
 
+    converted_equation = convert_double_at_sign_uuidv4s_into_square_brackets(uuid_v4s, converted_equation)
+
+
+    // Now make all square bracket ids not findable by non_square_bracket_ids_regex
+    // (I can't figure out how to modify the non_square_bracket_ids_regex to achieve this)
+    // As some like the uuid will match on this:
+    //     [10000000-0000-4000-a000-000000000000]
+    //                         ^^^^
+    // And some like ids with spaces will match:
+    //     [ some id ]
+    //       ^^^^ ^^
+    let hidden_ids
+    ({ hidden_ids, converted_equation } = hide_all_square_bracket_ids(converted_equation))
+
+    converted_equation = wrap_all_non_square_bracket_ids_in_square_brackets(converted_equation)
+
+    converted_equation = unhide_all_hidden_ids(hidden_ids, converted_equation)
+
+    return converted_equation
+}
+
+
+function convert_double_at_sign_uuidv4s_into_square_brackets (uuid_v4s: string[], converted_equation: string)
+{
     uuid_v4s.forEach(id =>
     {
         const replacer = new RegExp(`@@${id}`, "g")
@@ -26,28 +50,39 @@ export function normalise_calculation_ids (equation: string, uuid_v4s: string[])
         converted_equation = converted_equation.replace(replacer, replacement_content)
     })
 
+    return converted_equation
+}
 
-    // Now make all square bracket ids not findable by non_square_bracket_ids_regex
-    // (I can't figure out how to do this with a regex)
-    // As some like the uuid will match on this:
-    //     [10000000-0000-4000-a000-000000000000]
-    //                         ^^^^
-    // And some like ids with spaces will match:
-    //     [ some id ]
-    //       ^^^^ ^^
+
+function hide_all_square_bracket_ids (converted_equation: string)
+{
+
     const ids_to_hide = get_spaced_square_bracket_ids_from_text(converted_equation)
+
     ids_to_hide.forEach((id, index) =>
     {
         const replacer = new RegExp(`\\[${id}\\]`, "g")
 
-        const replacement_content = `[id_to_hide${index}]`
+        const replacement_content = `[hidden_id${index}]`
 
         converted_equation = converted_equation.replace(replacer, replacement_content)
     })
 
+    return { hidden_ids: ids_to_hide, converted_equation }
+}
 
+function get_spaced_square_bracket_ids_from_text (text: string): string[]
+{
+    const matches = [ ...text.matchAll(square_bracket_ids_regex) ]
+    return matches.map(entry => entry[1]!)
+}
+
+
+function wrap_all_non_square_bracket_ids_in_square_brackets (converted_equation: string)
+{
     const potential_non_square_bracket_ids = get_non_square_bracket_ids_from_text(converted_equation)
     const non_square_bracket_ids = exclude_reserved_simulationjs_words(potential_non_square_bracket_ids)
+
     non_square_bracket_ids.forEach(id =>
     {
         const replacer = new RegExp(`(^|[^\\[])${id}`, "g")
@@ -57,28 +92,8 @@ export function normalise_calculation_ids (equation: string, uuid_v4s: string[])
         converted_equation = converted_equation.replace(replacer, replacement_content)
     })
 
-
-    ids_to_hide.forEach((id, index) =>
-    {
-        const replacer = new RegExp(`\\[id_to_hide${index}\\]`, "g")
-
-        const replacement_content = `[${id}]`
-
-        converted_equation = converted_equation.replace(replacer, replacement_content)
-    })
-
-
     return converted_equation
 }
-
-
-
-function get_spaced_square_bracket_ids_from_text (text: string): string[]
-{
-    const matches = [ ...text.matchAll(square_bracket_ids_regex) ]
-    return matches.map(entry => entry[1]!)
-}
-
 
 function get_non_square_bracket_ids_from_text (text: string): string[]
 {
@@ -86,8 +101,21 @@ function get_non_square_bracket_ids_from_text (text: string): string[]
     return matches.map(entry => entry[1]!)
 }
 
-
 function exclude_reserved_simulationjs_words (potential_non_square_bracket_ids: string[])
 {
     return potential_non_square_bracket_ids.filter(id => !SIMULATION_JS_RESERVED_WORDS.has(id.toLowerCase()))
+}
+
+
+function unhide_all_hidden_ids (hidden_ids: string[], converted_equation: string)
+{
+    hidden_ids.forEach((id, index) =>
+    {
+        const replacer = new RegExp(`\\[hidden_id${index}\\]`, "g")
+
+        const replacement_content = `[${id}]`
+
+        converted_equation = converted_equation.replace(replacer, replacement_content)
+    })
+    return converted_equation
 }
