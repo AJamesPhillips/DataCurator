c6be8b17732c8c89fdb2e972fa3335f933cb9f43
Alexander James Phillips <ajp.global@gmail.com>
2023-09-29 16:59:19 +0100

    Add comments on VAPsType vs WComponentStateV2SubType

--- app/frontend/src/wcomponent/interfaces/VAPsType.ts
+++ app/frontend/src/wcomponent/interfaces/VAPsType.ts
@@ -1,6 +1,10 @@
 
 
-
+// VAPsType and WComponentStateV2SubType are two different types because
+// `WComponentNodeAction` has VAP sets and thus we need VAPsType.action but as it
+// is not WComponentNodeStateV2 we don't need a WComponentNodeStateV2.subtype
+// of "actions".  Also see the function `get_wcomponent_VAPs_represent` for an
+// example of the relationship between VAPsType and WComponentStateV2SubType.
 export enum VAPsType {
     boolean,
     number,
--- app/frontend/src/wcomponent/interfaces/state.ts
+++ app/frontend/src/wcomponent/interfaces/state.ts
@@ -21,6 +21,11 @@ export interface HasValuePossibilities
 }
 
 
+// VAPsType and WComponentStateV2SubType are two different types because
+// `WComponentNodeAction` has VAP sets and thus we need VAPsType.action but as it
+// is not WComponentNodeStateV2 we don't need a WComponentNodeStateV2.subtype
+// of "actions".  Also see the function `get_wcomponent_VAPs_represent` for an
+// example of the relationship between VAPsType and WComponentStateV2SubType.
 export type WComponentStateV2SubType = "boolean" | "number" /*| "datetime"*/ | "other"
 export interface WComponentNodeStateV2 extends WComponentNodeBase
 {
