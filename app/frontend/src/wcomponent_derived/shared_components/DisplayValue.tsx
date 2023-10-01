5f6c31f0d4384d38263dd3594dd642d57fd0164a
Alexander James Phillips <ajp.global@gmail.com>
2023-10-01 23:25:56 +0100

    Change colour of state value when using a derived value
    e.g. from a StateValue component causes a StateV2 component to have a different VAPSets

--- app/frontend/src/wcomponent_derived/shared_components/DisplayValue.tsx
+++ app/frontend/src/wcomponent_derived/shared_components/DisplayValue.tsx
@@ -12,9 +12,15 @@ interface OwnProps
 
 export function DisplayValue (props: OwnProps)
 {
-    const { UI_value: { values_string, counterfactual_applied, uncertain } } = props
+    const { UI_value } = props
+    const { values_string, counterfactual_applied, uncertain, derived__using_value_from_wcomponent_id } = UI_value
+    const assumption_or_other_forced_value = counterfactual_applied || !!derived__using_value_from_wcomponent_id
 
-    const class_name = `value ${counterfactual_applied ? "assumption" : ""} ${uncertain ? "uncertain" : ""}`
+    // TODO we need to re-work the colouring of this now because before the
+    // counterfactuals that showed up as assumption and cause the value to show as orange
+    // never had uncertainty, but now with state_value being included in this then it can be
+    // uncertain as well as `assumption_or_other_forced_value` being true.
+    const class_name = `value ${assumption_or_other_forced_value ? "assumption" : ""} ${uncertain ? "uncertain" : ""}`
 
     return <span className={class_name}>{values_string}</span>
 }
