e37b9c8f71cda0c946a052c1f859f62b0edee644
Alexander James Phillips <ajp.global@gmail.com>
2023-10-05 15:33:38 +0100

    Simplify get_wcomponent_VAPs_represent implementation

--- app/frontend/src/wcomponent/get_wcomponent_VAPs_represent.ts
+++ app/frontend/src/wcomponent/get_wcomponent_VAPs_represent.ts
@@ -3,7 +3,6 @@ import {
     wcomponent_is_allowed_to_have_state_VAP_sets,
     wcomponent_is_statev2,
     wcomponent_is_action,
-    wcomponent_is_causal_link,
     WComponentsById,
     wcomponent_is_state_value,
 } from "./interfaces/SpecialisedObjects"
@@ -16,36 +15,32 @@ export function get_wcomponent_VAPs_represent (wcomponent: WComponent | undefine
 {
     let VAPs_represent = VAPsType.undefined
 
-
-    if (_wcomponent_ids_touched.has(wcomponent?.id || ""))
+    do
     {
-        console .log(`Recursion prevented in "get_wcomponent_VAPs_represent" for wcomponent id: "${wcomponent?.id}" type: "${wcomponent?.type}"`)
-        return VAPs_represent
-    }
-    if (wcomponent) _wcomponent_ids_touched.add(wcomponent.id)
+        if (_wcomponent_ids_touched.has(wcomponent?.id || ""))
+        {
+            console .log(`Recursion prevented in "get_wcomponent_VAPs_represent" for wcomponent id: "${wcomponent?.id}" type: "${wcomponent?.type}"`)
+            break
+        }
+        if (wcomponent) _wcomponent_ids_touched.add(wcomponent.id)
 
 
-    if (!wcomponent_is_allowed_to_have_state_VAP_sets(wcomponent)) return VAPs_represent
+        if (!wcomponent_is_allowed_to_have_state_VAP_sets(wcomponent)) break
 
 
-    VAPs_represent = VAPsType.other
-    if (wcomponent_is_statev2(wcomponent)) VAPs_represent = subtype_to_VAPsType(wcomponent.subtype)
-    else if (wcomponent_is_action(wcomponent)) VAPs_represent = VAPsType.action
-    else if (wcomponent_is_causal_link(wcomponent))
-    {
-        // does this make sense? When is the VAPsType of a causal link used?
-        VAPs_represent = VAPsType.boolean
-    }
-    else if (wcomponent_is_state_value(wcomponent))
-    {
-        const attribute_wcomponent = wcomponents_by_id[wcomponent.attribute_wcomponent_id || ""]
-        if (attribute_wcomponent) VAPs_represent = get_wcomponent_VAPs_represent(attribute_wcomponent, wcomponents_by_id, _wcomponent_ids_touched)
-    }
-    else
-    {
-        console.error(`Unimplmented "get_wcomponent_VAPs_represent" for wcomponent id: "${wcomponent.id}" type: "${wcomponent.type}"`)
-    }
+        if (wcomponent_is_statev2(wcomponent)) VAPs_represent = subtype_to_VAPsType(wcomponent.subtype)
+        else if (wcomponent_is_action(wcomponent)) VAPs_represent = VAPsType.action
+        else if (wcomponent_is_state_value(wcomponent))
+        {
+            const attribute_wcomponent = wcomponents_by_id[wcomponent.attribute_wcomponent_id || ""]
+            if (attribute_wcomponent) VAPs_represent = get_wcomponent_VAPs_represent(attribute_wcomponent, wcomponents_by_id, _wcomponent_ids_touched)
+        }
+        else
+        {
+            console.error(`Unimplemented "get_wcomponent_VAPs_represent" for wcomponent id: "${wcomponent.id}" type: "${wcomponent.type}"`)
+        }
 
+    } while (false)
 
     return VAPs_represent
 }
