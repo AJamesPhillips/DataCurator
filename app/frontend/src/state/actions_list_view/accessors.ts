aa034ea4d8f9ca16fc2f77576340e1c7507d31da
Alexander James Phillips <ajp.global@gmail.com>
2023-12-07 09:36:40 +0000

    Revert "Use composed_visible_wc_id_map to populate actions list"

--- app/frontend/src/state/actions_list_view/accessors.ts
+++ app/frontend/src/state/actions_list_view/accessors.ts
@@ -6,3 +6,23 @@ export function get_is_on_actions_list_view (state: RootState)
 {
     return state.routing.args.view === "actions_list"
 }
+
+
+
+export function get_action_ids_for_actions_list_view (state: RootState)
+{
+    let action_ids: Set<string> | undefined = undefined
+
+    const filter_by_knowledge_view = false
+    if (filter_by_knowledge_view)
+    {
+        // const composed_knowledge_view = get_current_composed_knowledge_view_from_state(state)
+        // if (composed_knowledge_view)
+        // {
+        //     action_ids = composed_knowledge_view.wc_ids_by_type.action
+        // }
+    }
+    else action_ids = state.derived.wcomponent_ids_by_type.action
+
+    return action_ids
+}
