939f5739271b2ddf1156d7ee2f98bcc7f735bdd7
Alexander James Phillips <ajp.global@gmail.com>
2023-12-09 23:07:02 +0000

    Filter action list ids by current composed knowledge view

--- app/frontend/src/state/actions_list_view/accessors.ts
+++ app/frontend/src/state/actions_list_view/accessors.ts
@@ -1,4 +1,5 @@
 import { RootState } from "../State"
+import { get_current_composed_knowledge_view_from_state } from "../specialised_objects/accessors"
 
 
 
@@ -11,18 +12,17 @@ export function get_is_on_actions_list_view (state: RootState)
 
 export function get_action_ids_for_actions_list_view (state: RootState)
 {
-    let action_ids: Set<string> | undefined = undefined
+    let action_ids: Set<string> = state.derived.wcomponent_ids_by_type.action
 
-    const filter_by_knowledge_view = false
-    if (filter_by_knowledge_view)
+    const { filter_by_current_knowledge_view } = state.filter_context.filters
+    if (filter_by_current_knowledge_view)
     {
-        // const composed_knowledge_view = get_current_composed_knowledge_view_from_state(state)
-        // if (composed_knowledge_view)
-        // {
-        //     action_ids = composed_knowledge_view.wc_ids_by_type.action
-        // }
+        const composed_knowledge_view = get_current_composed_knowledge_view_from_state(state)
+        if (composed_knowledge_view)
+        {
+            action_ids = composed_knowledge_view.wc_ids_by_type.action
+        }
     }
-    else action_ids = state.derived.wcomponent_ids_by_type.action
 
     return action_ids
 }
