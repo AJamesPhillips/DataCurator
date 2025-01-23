4197d85afc01084b7f0f420c5c9dbeaaba45d4b7
Alexander James Phillips <ajp.global@gmail.com>
2025-01-23 13:13:02 +0000

    Update comment and add SQL to correct for bug with extraneous 'All' knowledge view creation

--- app/frontend/src/state/specialised_objects/ensure_a_knowledge_view_is_in_existence.ts
+++ app/frontend/src/state/specialised_objects/ensure_a_knowledge_view_is_in_existence.ts
@@ -21,9 +21,13 @@ export function ensure_a_knowledge_view_is_in_existence (store: StoreType)
         return
     }
 
-    // This is still buggy, as it may create a new knowledge view when (I think)
-    // the user's browser is forcibly refreshed to get a refreshed JWT token
-    // from supabase.
+    // This is still buggy as of 2025-01-23.  It creates a new knowledge view
+    // when (I think) the user's browser is forcibly refreshed to get a new
+    // JWT token from supabase.  So it's likely that the `state.sync.ready_for_reading`
+    // is not being correctly set to false rather than the `state.derived.knowledge_views`
+    // being incorrectly cleared (as this is a good defensive move to avoid a
+    // user signing out and back in with a different user and keeping the old
+    // content in `state.derived.knowledge_views`).
     const knowledge_view = get_new_knowledge_view_object({ title: "All", base_id }, state.creation_context)
     store.dispatch(ACTIONS.specialised_object.upsert_knowledge_view({ knowledge_view }))
 }
