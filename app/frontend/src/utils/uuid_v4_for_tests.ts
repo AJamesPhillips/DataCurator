e242ee669334bdc92a2ebc8c1bfde089153c2777
Alexander James Phillips <ajp.global@gmail.com>
2025-05-01 11:23:24 +0100

    uuid_v4_for_tests allows all numbers

--- app/frontend/src/utils/uuid_v4_for_tests.ts
+++ app/frontend/src/utils/uuid_v4_for_tests.ts
@@ -4,7 +4,7 @@
 export function uuid_v4_for_tests (id: number)
 {
     if (!Number.isInteger(id)) throw new Error("uuid_v4_for_tests id must be an integer")
-    if (id < 0 || id > 10) throw new Error("uuid_v4_for_tests id must be 0-9 inclusive")
+    if (id < 0) throw new Error("uuid_v4_for_tests id must be >=0")
 
     return `${id}0000000-0000-4000-a000-000000000000`
 }
