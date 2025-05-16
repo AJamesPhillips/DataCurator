7da8eb313c75cd7cf2f158dc3b0090a1e49ec68e
Alexander James Phillips <ajp.global@gmail.com>
2025-05-16 19:10:11 +0100

    Simplify AboutSidePanel

--- app/frontend/src/about/AboutSidePanel.tsx
+++ app/frontend/src/about/AboutSidePanel.tsx
@@ -1,29 +1,8 @@
-import { FunctionComponent } from "preact"
-import { connect, ConnectedProps } from "react-redux"
-
-import type { RootState } from "../state/State"
 
 
-
-interface OwnProps {}
-
-const map_state = (state: RootState) =>
-{
-    return {}
-}
-
-
-
-const connector = connect(map_state)
-type Props = ConnectedProps<typeof connector> & OwnProps
-
-
-function _AboutSidePanel (props: Props)
+export function AboutSidePanel ()
 {
     return <div>
-        <span className="description_label">Version</span> <b>2025-05-13</b>
+        <span className="description_label">Version</span> <b>2025-05-16</b>
     </div>
 }
-
-
-export const AboutSidePanel = connector(_AboutSidePanel) as FunctionComponent<OwnProps>
