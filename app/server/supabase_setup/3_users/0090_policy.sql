0b298271c3f2d2871c89927f4e310d2f62244c5f
Alexander James Phillips <ajp.global@gmail.com>
2025-10-07 11:36:40 +0100

    Simplify and correct user RLS policies

--- app/server/supabase_setup/3_users/0090_policy.sql
+++ app/server/supabase_setup/3_users/0090_policy.sql
@@ -4,14 +4,7 @@
 alter table public.users enable row level security;
 
 
-CREATE policy "User can read their own user entry" on public.users for select using ( users.id = auth.uid() );
-CREATE policy "User can insert their own user entry" on public.users for insert with check ( users.id = auth.uid() );
-CREATE policy "User can update their own user entry" on public.users for update using ( users.id = auth.uid() ) with check ( users.id = auth.uid() );
--- This allows you to go from knowing someone's user id or email to having access to their user table.
--- This sounds like a bad idea but for the moment we will let it pass as it's own the user name.
--- SECURITY
-CREATE policy "User can read other users entries via owned bases" on public.users for select using ( users.id in (select get_owned_access_control_user_ids_for_authorised_user()) );
+CREATE policy "User can insert their own user entry" on public.users for insert with check ( users.id = (select auth.uid()) );
+CREATE policy "User can update their own user entry" on public.users for update using ( users.id = (select auth.uid()) ) with check ( users.id = (select auth.uid()) );
 
-CREATE policy "Other users can read fellow users entries" on public.users for select using ( users.id in (select get_fellow_access_control_user_ids_for_authorised_user()) );
-CREATE policy "Other users can read owner user entry" on public.users for select using ( users.id in (select get_owner_user_ids_for_authorised_user()) );
-CREATE policy "Other users can read user entries of public bases" on public.users for select using ( users.id in (select owner_user_ids_of_public_bases()) );
+CREATE policy "User can read all users" on public.users for select using ( true );
